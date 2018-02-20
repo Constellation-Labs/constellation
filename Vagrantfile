84cb5953e9419b9e9364d32be31cfd7bfc7125ab
Preston Parris <prestonparris@gmail.com>
2018-02-19 19:44:01 -0800

    Fix for minikube init.

--- Vagrantfile
+++ Vagrantfile
@@ -3,11 +3,13 @@ Vagrant.configure("2") do |config|
 
   config.vm.hostname = 'constellation'
 
-  config.vm.network "private_network", ip: "192.168.33.77"
+  config.vm.network "private_network", type: "dhcp"
+  config.vm.network "forwarded_port", guest: 8080, host: 8080
+  config.vm.network "forwarded_port", guest: 8001, host: 8001
 
   project_root = File.dirname(__FILE__)
 
-  config.vm.synced_folder project_root, "/home/vagrant/constellation"
+  config.vm.synced_folder project_root, "/home/vagrant/constellation", type: "nfs"
 
   config.ssh.forward_agent = true
 
--- start-minikube.sh
+++ start-minikube.sh
@@ -1,3 +1,25 @@
-vagrant ssh -c 'sudo kubectl proxy --port=8080 &'
 vagrant ssh -c 'export CHANGE_MINIKUBE_NONE_USER=true; sudo minikube start --vm-driver none'
+vagrant ssh -c '
+# this for loop waits until kubectl can access the api server that Minikube has created
+for i in {1..150}; do # timeout for 5 minutes
+   ./kubectl get po &> /dev/null
+   if [ $? -ne 1 ]; then
+      break
+  fi
+  sleep 2
+done'
+# doing this temporarily since there is currently an issue with minikube init
+# https://github.com/kubernetes/minikube/issues/2479
+vagrant ssh -c 'sudo minikube delete'
+vagrant ssh -c 'sudo minikube start --vm-driver none'
+vagrant ssh -c '
+# this for loop waits until kubectl can access the api server that Minikube has created
+for i in {1..150}; do # timeout for 5 minutes
+   ./kubectl get po &> /dev/null
+   if [ $? -ne 1 ]; then
+      break
+  fi
+  sleep 2
+done'
 vagrant ssh -c 'sudo kubectl config use-context minikube'
+vagrant ssh -c 'sudo kubectl proxy --port=8080 &'
