de50469ce96fe747ace98d7261d1bd121a184593
buckysballs <wlmeldmanfloch@me.com>
2018-02-26 12:41:09 -0800

    add interface/singleton

---
+++ src/main/scala/org/constellation/hufflepuff/Honeybadger.scala
@@ -0,0 +1,15 @@
+package org.constellation.hufflepuff
+
+/**
+  * Interface for an actor that wishes to perform cryptographic consensus
+  */
+trait Honeybadger {
+
+}
+
+/**
+  * Pure functions
+  */
+object Honeybadger {
+
+}
