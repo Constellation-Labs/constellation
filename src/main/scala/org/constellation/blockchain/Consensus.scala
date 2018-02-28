2a0371123534003249a142198a86128bca05c383
buckysballs <wlmeldmanfloch@me.com>
2018-02-28 15:35:53 -0800

    clean up obj and interface

--- src/main/scala/org/constellation/blockchain/Consensus.scala
+++ src/main/scala/org/constellation/blockchain/Consensus.scala
@@ -2,15 +2,22 @@ package org.constellation.blockchain
 
 import akka.actor.{ActorRef, FSM}
 import akka.actor.FSM.State
-import org.constellation.actor.Receiver
-import org.constellation.blockchain.Consensus.PerformConsensus
-import org.constellation.p2p.PeerToPeer
-import org.constellation.rpc.ProtocolInterface
-
 import scala.collection.mutable
 
 /**
-  * Ideally I'd like this to be instantiated as a Metamorphism.
+  * Interface for Consensus. This needs to be its own actor and have a type corresponding to the metamorphism that
+  * implements proof of meme. The actual rep/dht will be dispatched via this actor.
+  *
+  * We need to implement this to follow the signature below
+  *
+  *
+  * def metaSimple[F[_] : Functor, A, B](f: F[B] => B)(g: A => F[A]): A => B =
+  *   cata(g) andThen ana(f)
+  *
+  * and also figure out a way to define the algebras in terms of a bialgebra given that http://comonad.com/reader/2009/recursion-schemes/
+  * and we also need a vector space for proof of meme https://en.wikipedia.org/wiki/Bialgebra
+  *
+  * for ref http://www.cs.ox.ac.uk/jeremy.gibbons/publications/metamorphisms-scp.pdf
   */
 object Consensus {
   case class PerformConsensus()
@@ -23,21 +30,4 @@ object Consensus {
   def performConsensus(actorRefs: Seq[ActorRef]): CheckpointBlock = CheckpointBlock("hashPointer", 0L, "signature", mutable.HashMap[ActorRef, Option[BlockData]](), 0L)
 }
 
-/**
-  * Interface for Consensus. This needs to be its own actor and have a type corresponding to the metamorphism that
-  * implements proof of meme. The actual rep/dht will be dispatched via this actor.
-  *
-  * We need to implement this to follow the signature below
-  *
-  *
-  * def metaSimple[F[_] : Functor, A, B](f: F[B] => B)(g: A => F[A]): A => B =
-      cata(g) andThen ana(f)
-  *
-  * and also figure out a way to define the algebras in terms of a bialgebra given that http://comonad.com/reader/2009/recursion-schemes/
-  * and we also need a vector space for proof of meme https://en.wikipedia.org/wiki/Bialgebra
-  *
-  * for ref http://www.cs.ox.ac.uk/jeremy.gibbons/publications/metamorphisms-scp.pdf
-  */
-class Consensus[T,U](chain: DAG) extends FSM[State[T,U], Unit] {
-
-}
+class Consensus[T,U](chain: DAG) extends FSM[State[T,U], Unit]
