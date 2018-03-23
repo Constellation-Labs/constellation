7bde4106476a4125cd84a45f1ced36322e5f1e6f
Preston Parris <prestonparris@gmail.com>
2018-03-22 18:13:32 -0700

    add more consensus tests

--- src/test/scala/org/constellation/consensus/ConsensusTest.scala
+++ src/test/scala/org/constellation/consensus/ConsensusTest.scala
@@ -26,23 +26,41 @@ class ConsensusTest extends TestKit(ActorSystem("ConsensusTest")) with FlatSpecL
   }
 
   "getFacilitators" should "give back the correct list of facilitators" in {
+    val node1 = TestProbe()
+    val node2 = TestProbe()
+    val node3 = TestProbe()
+    val node4 = TestProbe()
 
-    val block = Block("hashPointer", 0L, "sig", Seq(), 0L, Seq())
+    val membersOfCluster = Seq(node1.ref, node2.ref, node3.ref, node4.ref)
+
+    val block = Block("hashPointer", 0L, "sig", membersOfCluster, 0L, Seq())
 
     val facilitators = Consensus.getFacilitators(block)
 
-    assert(facilitators == Seq())
+    // TODO: modify once we have subset filtering logic
+    val expectedFacilitators = Seq(node1.ref, node2.ref, node3.ref, node4.ref)
+
+    assert(facilitators == expectedFacilitators)
   }
 
   "notifyFacilitatorsOfBlockProposal" should "notify all of the correct facilitators with the correct block" in {
 
     val block = Block("hashPointer", 0L, "sig", Seq(), 0L, Seq())
 
-    val testProbe = TestProbe()
+    val self = TestProbe()
 
-    val notify = Consensus.notifyFacilitatorsOfBlockProposal(block, Seq(), testProbe.ref)
+    val peer1 = TestProbe()
+    val peer2 = TestProbe()
+    val peer3 = TestProbe()
 
-    // TODO add assertions
+    val notify = Consensus.notifyFacilitatorsOfBlockProposal(block, Seq(peer1.ref, peer2.ref, peer3.ref), self.ref)
+
+    val peerProposedBlock = PeerProposedBlock(block, self.ref)
+
+    peer1.expectMsg(peerProposedBlock)
+    peer2.expectMsg(peerProposedBlock)
+    peer3.expectMsg(peerProposedBlock)
+
   }
 
   "isFacilitator" should "return correctly if the actor is a facilitator" in {
@@ -78,7 +96,7 @@ class ConsensusTest extends TestKit(ActorSystem("ConsensusTest")) with FlatSpecL
 
     // TODO add assertions
   }
-  
+
   "A PeerProposedBlock Message" should "be handled correctly" in new WithConsensusActor {
     val peerProbe = TestProbe()
 
