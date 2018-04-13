3bbb53552daed565dec9bcac59542076ed805b05
Preston Parris <prestonparris@gmail.com>
2018-04-13 14:01:41 -0700

    cleanup

--- src/test/scala/org/constellation/Fixtures.scala
+++ src/test/scala/org/constellation/Fixtures.scala
@@ -13,9 +13,6 @@ object Fixtures {
 
   val tempKey: KeyPair = KeyUtils.makeKeyPair()
   val tx = Transaction(0L, tempKey.getPublic, tempKey.getPublic, 1L)
-  // val signTx = SignTransaction("", "", "" , 1L, "")
-//  val checkpointBlock = primitives.Block.Block("hashPointer", 0L, "signature", mutable.HashMap.empty[ActorRef, Option[Block]], 0L)
-//  val genesisBlock = primitives.Block.Block("genesisBlock", 0L, "signature", mutable.HashMap.empty[ActorRef, Option[Block]], 0L)
   val publicKey: PublicKey = tempKey.getPublic
 
 }
