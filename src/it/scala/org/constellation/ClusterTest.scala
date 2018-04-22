03d0585026f2d59d6b087cc57df108ffbd732279
ryle <ryle.goehausen@gmail.com>
2018-04-22 10:58:11 -0700

    Fix loop

--- src/it/scala/org/constellation/ClusterTest.scala
+++ src/it/scala/org/constellation/ClusterTest.scala
@@ -5,7 +5,7 @@ import akka.stream.ActorMaterializer
 import akka.testkit.TestKit
 import org.constellation.util.RPCClient
 import org.json4s.JsonAST.JArray
-import org.scalatest.{BeforeAndAfterAll, FlatSpec, FlatSpecLike}
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike}
 
 import scala.concurrent.ExecutionContextExecutor
 import constellation._
@@ -31,7 +31,7 @@ class ClusterTest extends TestKit(ActorSystem("ClusterTest")) with FlatSpecLike 
     val cmd = {if (isCircle) Seq("sudo", "/opt/google-cloud-sdk/bin/kubectl") else Seq("kubectl")} ++
       Seq("--output=json", "get", "services")
     val result = cmd.!!
-    // println(s"GetIP Result: $result")
+     println(s"GetIP Result: $result")
     val items = (result.jValue \ "items").extract[JArray]
     val ips = items.arr.filter{ i =>
       (i \ "metadata" \ "name").extract[String].startsWith("rpc")
@@ -51,9 +51,9 @@ class ClusterTest extends TestKit(ActorSystem("ClusterTest")) with FlatSpecLike 
 
       while (!done) {
         i += 1
-        Thread.sleep(5000)
+        Thread.sleep(30000)
         val t = Try{getIPs}
-        println(s"Waiting for machines to come online $t ${i * 5} seconds")
+        println(s"Waiting for machines to come online $t ${i * 30} seconds")
         done = t.toOption.exists { ps =>
           val validIPs = ps.forall { ip =>
             val res = "\\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\b".r.findAllIn(ip)
@@ -62,14 +62,16 @@ class ClusterTest extends TestKit(ActorSystem("ClusterTest")) with FlatSpecLike 
           ps.lengthCompare(3) == 0 && validIPs
         }
       }
+      Thread.sleep(10000)
     }
 
     val ips = getIPs
 
+
     val rpcs = ips.map{
       ip =>
-        val r = new RPCClient(ip, 9000 )
-        assert(r.getBlockingStr[String]("health") == "OK")
+        val r = new RPCClient(ip, 9000)
+        assert(r.getBlockingStr[String]("health", timeout = 100) == "OK")
         println(s"Health ok on $ip")
         assert(r.post("ip", ip + ":16180").get().unmarshal.get() == "OK")
         r
