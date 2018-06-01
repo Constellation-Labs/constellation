fddaaa85025620995c0270df985a85e66c4531b3
Preston Parris <prestonparris@gmail.com>
2018-06-01 12:41:18 -0700

    cleanup udp warnings

--- src/main/scala/org/constellation/p2p/PeerToPeer.scala
+++ src/main/scala/org/constellation/p2p/PeerToPeer.scala
@@ -12,7 +12,7 @@ import akka.util.{ByteString, Timeout}
 import com.typesafe.scalalogging.Logger
 import constellation._
 import org.constellation.LevelDB
-import org.constellation.consensus.Consensus.{Checkpoint, Conflict, ConsensusProposal, ConsensusVote}
+import org.constellation.consensus.Consensus._
 import org.constellation.p2p.PeerToPeer._
 import org.constellation.primitives.Chain.Chain
 import org.constellation.primitives.Schema.{Gossip, TX}
@@ -370,22 +370,9 @@ class PeerToPeer(
           udpActor ! UDPSendTyped(dataA, r.data.externalAddress)
       }
 
-    // Add type bounds here on all the command forwarding types
-    // I.e. PeerMemPoolUpdated extends ConsensusCommand
-    // Just check on ConsensusCommand and send to consensus actor automatically
-    case UDPMessage(p: ConsensusVote[Conflict], remote) =>
-      //  logger.debug("UDP PeerMemPoolUpdated received")
-      consensusActor ! p
-
-    case UDPMessage(p: ConsensusVote[Checkpoint], remote) =>
-      //  logger.debug("UDP PeerMemPoolUpdated received")
-      consensusActor ! p
-
-    case UDPMessage(p : ConsensusProposal[Conflict], remote) =>
-      consensusActor ! p
-
-    case UDPMessage(p : ConsensusProposal[Checkpoint], remote) =>
-      consensusActor ! p
+    case UDPMessage(message: RemoteMessage, remote) => {
+      consensusActor ! message
+    }
 
     case UDPMessage(sh: HandShakeResponseMessage, remote) =>
       //    logger.debug(s"HandShakeResponseMessage from $remote on $externalAddress second remote: $remote")
