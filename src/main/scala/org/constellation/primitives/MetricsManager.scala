151873a41823746d1150f254933b7f889cd59aa4
Tyler Prete <tyler@constellationlabs.io>
2018-07-25 17:47:42 -0700

    Generally want to use become pattern to handle actor state. (#92)
    Using var / mutable objects might be more performant for data that
    is changed very frequently -- but generally I like this pattern better.
    
    Happy to discuss -- just wanted to provide some examples to demonstrate the pattern.

--- src/main/scala/org/constellation/primitives/MetricsManager.scala
+++ src/main/scala/org/constellation/primitives/MetricsManager.scala
@@ -2,8 +2,6 @@ package org.constellation.primitives
 
 import akka.actor.Actor
 
-import scala.collection.mutable
-
 case object GetMetrics
 
 case class UpdateMetric(key: String, value: String)
@@ -12,16 +10,18 @@ case class IncrementMetric(key: String)
 
 class MetricsManager extends Actor {
 
-  private val metrics = mutable.HashMap[String, String]()
+  override def receive = active(Map.empty)
 
-  override def receive: Receive = {
+  def active(metrics: Map[String, String]): Receive = {
 
-    case GetMetrics => sender() ! metrics.toMap
+    case GetMetrics => sender() ! metrics
 
-    case UpdateMetric(key, value) => metrics(key) = value
+    case UpdateMetric(key, value) => context become active(metrics + (key -> value))
 
     case IncrementMetric(key) =>
-      metrics(key) = metrics.get(key).map{z => (z.toLong + 1).toString}.getOrElse("1")
+      // Why are the values strings if we're just going to convert back and forth from longs?
+      val updatedMap = metrics + (key -> metrics.get(key).map{z => (z.toLong + 1).toString}.getOrElse("1"))
+      context become active(updatedMap)
 
   }
 }
