7a65c746779b63bac7e62120e34c4391988086ec
Tyler Prete <psyonic@gmail.com>
2018-08-01 12:21:59 -0700

    Make paths relative.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 pushd ui
 sbt fullOptJS
 popd
-cp /Users/tyler/constellation/constellation/ui/target/scala-2.11/*js* src/main/resources/ui/
+cp ui/target/scala-2.11/*js* src/main/resources/ui/
 sbt run
