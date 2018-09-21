05cc0e31259223e3bcdb079216a5cfe29d8d5a2b
ryle-goehausen <ryle.goehausen@gmail.com>
2018-09-21 10:32:29 -0700

    wip

---
+++ src/test/scala/org/constellation/tx/TXValidationBenchmark.scala
@@ -0,0 +1,17 @@
+package org.constellation.tx
+
+import org.scalatest.FlatSpec
+
+class TXValidationBenchmark extends FlatSpec {
+
+
+  "Timing" should "validate transactions with timer" in {
+
+
+
+
+
+
+  }
+
+}
