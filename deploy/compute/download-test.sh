dd2200ea01e7b7531c8e8fbe62834a04b8239ce9
ryle-goehausen <ryle.goehausen@gmail.com>
2018-10-24 14:02:43 -0700

    Fix inconsistent quote usage

--- deploy/compute/download-test.sh
+++ deploy/compute/download-test.sh
@@ -8,6 +8,6 @@ cat $HOSTS_FILE
 sbt assembly && \
 pscp -h $HOSTS_FILE target/scala-2.12/constellation-assembly-1.0.1.jar /home/$USER/ && \
 pssh -h $HOSTS_FILE -i 'sudo killall java'
-pssh -h $HOSTS_FILE -i "rm -rf /home/$USER/tmp"
+pssh -h $HOSTS_FILE -i 'rm -rf /home/$USER/tmp'
 pssh -h $HOSTS_FILE -i 'java -jar ~/constellation-assembly-1.0.1.jar > ~/dag.log 2>&1 &' && \
 sbt "it:testOnly org.constellation.ClusterSingleDownloadJoinTest"
--- deploy/compute/redeploy.sh
+++ deploy/compute/redeploy.sh
@@ -10,7 +10,7 @@ gsutil cp target/scala-2.12/constellation-assembly-1.0.1.jar gs://constellation-
 gsutil acl ch -u AllUsers:R gs://constellation-dag/release/dag.jar && \
 pssh -h $HOSTS_FILE -i 'gsutil cp gs://constellation-dag/release/dag.jar .' && \
 pssh -h $HOSTS_FILE -i 'killall java'
-pssh -h $HOSTS_FILE -i "rm -rf /home/$USER/tmp"
+pssh -h $HOSTS_FILE -i 'rm -rf /home/$USER/tmp'
 pssh -h $HOSTS_FILE -i 'source ~/.ssh/environment; java -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.authenticate=false -Dm.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=$(cat external_host_ip) -Dcom.sun.management.jmxremote.local.only=false -jar ~/dag.jar > ~/dag.log 2>&1 &' && \
 echo "Done"
 sbt "it:testOnly org.constellation.ClusterComputeManualTest"
