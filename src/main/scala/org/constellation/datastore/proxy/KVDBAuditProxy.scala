7b98f62234855d94af27010ccb1f2e2f3e206e54
Tyler Prete <psyonic@gmail.com>
2018-10-26 12:15:37 -0700

    Add timing information to audit log.

--- src/main/scala/org/constellation/datastore/proxy/KVDBAuditProxy.scala
+++ src/main/scala/org/constellation/datastore/proxy/KVDBAuditProxy.scala
@@ -8,32 +8,39 @@ class KVDBAuditProxy(kvdb: KVDB) extends KVDB {
 
   private val f = File.newTemporaryFile("kvdb-audit")
 
+  def timer[T](f: => T): (T, Long) = {
+    val startTime = System.currentTimeMillis()
+    val res = f
+    val endTime = System.currentTimeMillis()
+    (res, endTime - startTime)
+  }
+
   f.createFileIfNotExists()
 
   override def put(key: String, obj: AnyRef): Boolean = {
-    val res = kvdb.put(key, obj)
-    f.appendLine(s"PUT $key ${obj.json} $res")
+    val (res, time) = timer { kvdb.put(key, obj) }
+    f.appendLine(s"PUT $key ${obj.json} $res ${time}ms")
     res
   }
 
   override def get[T <: AnyRef](key: String): Option[T] = {
-    val res = kvdb.get(key)
-    f.appendLine(s"GET $key ${res.json}")
+    val (res, time) = timer { kvdb.get(key) }
+    f.appendLine(s"GET $key ${res.json} ${time}ms")
     res
   }
 
   override def update[T <: AnyRef](key: String,
                                    updateF: T => T,
                                    empty: T): T = {
-    val before = kvdb.get(key)
-    val res = kvdb.update(key, updateF, empty)
-    f.appendLine(s"UPDATE $key ${res.json} (BEFORE UPDATE: ${before.json})")
+    val (before, t1) = timer { kvdb.get(key) }
+    val (res, time) = timer { kvdb.update(key, updateF, empty) }
+    f.appendLine(s"UPDATE $key ${res.json} ${time}ms (BEFORE UPDATE: ${before.json} ${t1}ms)")
     res
   }
 
   override def delete(key: String): Boolean = {
-    val res = kvdb.delete(key)
-    f.appendLine(s"DELETE $key")
+    val (res, time) = timer { kvdb.delete(key) }
+    f.appendLine(s"DELETE $key ${time}ms")
     res
   }
 
