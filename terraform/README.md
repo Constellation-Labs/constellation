099e2930765ceba36e940c2c6db25f09e7850829
Tyler Prete <psyonic@gmail.com>
2018-10-31 13:41:35 -0700

    Adding terraform files.

---
+++ terraform/README.md
@@ -0,0 +1,16 @@
+## Setup
+
+### Usage
+Something something terraform apply / terraform destroy
+
+### Credentials
+Locally, make sure this environment variable is set to your google credentials file:
+```export GOOGLE_APPLICATION_CREDENTIALS=${path to your file}```
+
+[Create and download a new credentials json](https://console.developers.google.com/apis/credentials?organizationId=802489480189&project=esoteric-helix-197319)
+
+### SSH USER
+By default, the terraform command will ask for the ssh user to use. If you'd prefer to avoid answering this every time, you can set the TF_VAR_ssh_user env variable.
+```export TF_VAR_ssh_user=${USER}``` is probably what you want.
+
+
---
+++ terraform/default/main.tf
@@ -0,0 +1,146 @@
+variable "ssh_user" {
+  type = "string"
+}
+
+variable "project_name" {
+  type    = "string",
+  default = "esoteric-helix-197319"
+}
+
+variable "region" {
+  type    = "string",
+  default = "us-west2"
+}
+
+// Configure the Google Cloud provider
+provider "google" {
+ project     = "${var.project_name}"
+ region      = "${var.region}"
+}
+
+// Terraform plugin for creating random ids
+resource "random_id" "instance_id" {
+ byte_length = 4
+}
+
+output "instance_ips" {
+  value = "${google_compute_instance.default.*.network_interface.0.access_config.0.assigned_nat_ip}"
+}
+
+// A single Google Cloud Engine instance
+resource "google_compute_instance" "default" {
+ count          = 2
+ name = "constellation-${random_id.instance_id.hex}-${count.index}"
+ machine_type   = "n1-standard-1"
+ zone           = "us-west2-b"
+ can_ip_forward = true
+
+
+ tags = ["constellation-vm-${random_id.instance_id.hex}"]
+
+ boot_disk {
+   initialize_params {
+     image = "ubuntu-os-cloud/ubuntu-1604-lts"
+   }
+   auto_delete = true
+ }
+
+// Make sure flask is installed on all new instances for later steps
+ metadata_startup_script = "sudo apt update; sudo apt install -yq build-essential haveged rsync google-cloud-sdk openjdk-8-jre-headless"
+
+ network_interface {
+   network = "${google_compute_network.default.name}"
+
+   access_config {
+     // Include this section to give the VM an external ip address
+   }
+ }
+
+ scheduling {
+  preemptible = "true" // "true"
+  automatic_restart = "false"
+ }
+
+  provisioner "file" {
+    source      = "setup.sh"
+    destination = "/tmp/setup.sh"
+
+    connection {
+      type = "ssh"
+      user = "${var.ssh_user}"
+    }
+  }
+
+  provisioner "file" {
+    source      = "constellation.service"
+    destination = "/tmp/constellation.service"
+
+    connection {
+      type = "ssh"
+      user = "${var.ssh_user}"
+    }
+  }
+
+
+  provisioner "file" {
+    source      = "start"
+    destination = "/tmp/start"
+
+    connection {
+      type = "ssh"
+      user = "${var.ssh_user}"
+    }
+  }
+
+  provisioner "file" {
+    source      = "dag"
+    destination = "/tmp/dag"
+
+    connection {
+      type = "ssh"
+      user = "${var.ssh_user}"
+    }
+  }
+
+ provisioner "remote-exec" {
+  inline = [
+    "chmod +x /tmp/setup.sh",
+    "/tmp/setup.sh"
+  ]
+  connection {
+    type = "ssh"
+    user = "${var.ssh_user}"
+  }
+ }
+
+  provisioner "remote-exec" {
+  inline = [
+    "sudo systemctl start constellation"
+  ]
+  connection {
+    type = "ssh"
+    user = "${var.ssh_user}"
+  }
+ }
+
+}
+
+resource "google_compute_network" "default" {
+  name = "dag-network"
+}
+
+resource "google_compute_firewall" "default" {
+ name    = "constellation-app-firewall"
+ network = "${google_compute_network.default.name}"
+
+ // enable_logging = true
+
+  allow {
+    protocol = "icmp"
+  }
+
+ allow {
+   protocol = "tcp"
+   ports    = ["22", "9000","9001","9010", "9011"]
+ }
+}
