2819c91865b11e641fe6d64d365a85f0ab797e50
Tyler Prete <psyonic@gmail.com>
2019-01-31 17:43:38 -0700

    Add additivity=false to APIClient logger config.
    This avoids duplicate logging.

--- src/main/scala/org/constellation/util/Simulation.scala
+++ src/main/scala/org/constellation/util/Simulation.scala
@@ -3,16 +3,19 @@ package org.constellation.util
 import java.util.concurrent.ForkJoinPool
 
 import com.softwaremill.sttp.Response
-import com.typesafe.scalalogging.StrictLogging
+import com.typesafe.scalalogging.{Logger, StrictLogging}
 import constellation._
 import org.constellation.primitives.Schema._
 import org.constellation.{HostPort, PeerMetadata}
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutorService, Future}
 import scala.util.{Random, Try}
 
-class Simulation extends StrictLogging {
+class Simulation {
+
+  val logger = Logger(LoggerFactory.getLogger(getClass.getName))
 
   implicit val ec: ExecutionContextExecutorService =
     ExecutionContext.fromExecutorService(new ForkJoinPool(100))
