fb9e3e6107508628706b81ef37e614fce97e74a1
buckysballs <wlmeldmanfloch@me.com>
2019-02-04 17:25:45 -0800

    add notes

--- src/main/scala/org/constellation/util/Partitioner.scala
+++ src/main/scala/org/constellation/util/Partitioner.scala
@@ -3,7 +3,36 @@ package org.constellation.util
 import com.google.common.hash.Hashing
 import org.constellation.primitives.Schema.{Id, Transaction}
 
+import scala.annotation.tailrec
+
 object Partitioner {
+
+  /*
+  for checkpoint blocks, choose facilitator based on who is responsible for the majority of txs in the cpb.
+   */
+  //def selectCpFacilitator
+  
+  /*
+  for exponential drop off
+   */
+  def log2(num: Int) = scala.math.log(num) / scala.math.log(2)
+
+  def gossip(ids: Seq[Id], tx: Transaction) = {
+    val gossipRounds = log2(ids.size).toInt
+    val path = propagationPath(ids, tx)(gossipRounds)
+  }
+
+  /*
+  this should be called once each time it is received by a node, to determine if it has been passed accouring to correct path
+   */
+  def propagationPath(ids: Seq[Id], tx: Transaction)(depth: Int): List[Id] = {
+    if (depth > 0) {
+      val facilitator: Id = selectTxFacilitator(ids, tx)
+      val nextFacilitator = propagationPath(ids.filterNot(_ == facilitator), tx)(depth - 1)
+      facilitator :: nextFacilitator
+    }
+    else Nil
+  }
 
   def numeric256(hash: Array[Byte]) = {
     val sha256 = Hashing.sha256.hashBytes(hash).asBytes()
