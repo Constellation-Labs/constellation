4e1ccae435ce26fded28a7dc0d7e628edf68a4b7
Marcin Wadoń <mwadon@evojam.com>
2019-02-21 10:23:49 +0100

    Consensus flow - self timeout

--- src/main/scala/org/constellation/consensus/Round.scala
+++ src/main/scala/org/constellation/consensus/Round.scala
@@ -1,17 +1,40 @@
 package org.constellation.consensus
-import akka.actor.{Actor, ActorLogging, Props}
+import akka.actor.{Actor, ActorLogging, Cancellable, Props}
+import org.constellation.primitives.{CheckpointBlock, Schema}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
 class Round(id: RoundId) extends Actor with ActorLogging {
+  var proposals: Map[FacilitatorId, CheckpointBlock] = Map()
 
   override def preStart(): Unit = {
     context.parent ! NotifyFacilitators(id)
 
     // TODO: pull transactions and broadcast proposal
     // dao.threadSafeTXMemPool.pull(dao.minCheckpointFormationThreshold)
+
   }
 
   override def receive: Receive = {
+    case ReceivedProposal(_, facilitatorId, cb) =>
+      storeProposal(facilitatorId, cb)
+
+    case UnionProposals(_) => unionProposals()
+
     case _ => log.info("Received unknown message")
+  }
+
+  def scheduleUnionProposals: Cancellable =
+    context.system.scheduler.scheduleOnce(15.seconds, self, UnionProposals(id))
+
+  def storeProposal(facilitatorId: FacilitatorId, cb: CheckpointBlock): Unit = {
+    proposals = proposals + (facilitatorId -> cb)
+  }
+
+  def unionProposals(): Unit = {
+    log.debug(proposals.toString())
+    log.info("Union proposals and broadcast majority unioned block")
   }
 
 }
--- src/main/scala/org/constellation/consensus/RoundCommand.scala
+++ src/main/scala/org/constellation/consensus/RoundCommand.scala
@@ -1,10 +1,10 @@
 package org.constellation.consensus
+import org.constellation.primitives.{CheckpointBlock, Schema}
 
+case class FacilitatorId(id: Schema.Id) extends AnyVal
 case class RoundId(id: String) extends AnyVal
 
 // TODO
-case class CheckpointBlockProposal()
-case class MajorityUnionedBlock()
 
 sealed trait RoundCommand {
   def roundId: RoundId
@@ -12,7 +12,8 @@ sealed trait RoundCommand {
 
 case class NotifyFacilitators(roundId: RoundId) extends RoundCommand
 case class BlockCreationRoundStarted(roundId: RoundId) extends RoundCommand
-case class BroadcastProposal(roundId: RoundId, cb: CheckpointBlockProposal) extends RoundCommand
-case class ReceivedProposal(roundId: RoundId, cb: CheckpointBlockProposal) extends RoundCommand
-case class BroadcastMajorityUnionedBlock(roundId: RoundId, cb: MajorityUnionedBlock) extends RoundCommand
-case class ReceivedMajorityUnionedBlock(roundId: RoundId, cb: MajorityUnionedBlock) extends RoundCommand
+case class BroadcastProposal(roundId: RoundId, cb: CheckpointBlock) extends RoundCommand
+case class ReceivedProposal(roundId: RoundId, facilitatorId: FacilitatorId, cb: CheckpointBlock) extends RoundCommand
+case class UnionProposals(roundId: RoundId) extends RoundCommand
+case class BroadcastMajorityUnionedBlock(roundId: RoundId, cb: CheckpointBlock) extends RoundCommand
+case class ReceivedMajorityUnionedBlock(roundId: RoundId, cb: CheckpointBlock) extends RoundCommand
