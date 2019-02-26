f94d9b766abb597e75ecd5342f94f3929dd9767f
Wojtek Koszycki <koszycki.wojciech@gmail.com>
2019-02-25 17:47:37 -0800

    Update deploy/compute/build-upload.sh
    Co-Authored-By: buckysballs <wlmeldmanfloch@me.com>

--- deploy/compute/build-upload.sh
+++ deploy/compute/build-upload.sh
@@ -3,5 +3,6 @@
 JAR_TAG=${1:-dev}
 
 sbt assembly && \
-gsutil cp target/scala-2.12/constellation-assembly-1.0.10.jar gs://constellation-dag/release/dag-$JAR_TAG.jar && \
+version="$(cat  version.sbt  | cut -d '-' -f 1 | cut -d '"' -f 2)"
+gsutil cp target/scala-2.12/constellation-assembly-$versionjar gs://constellation-dag/release/dag-$JAR_TAG.jar && \
 gsutil acl ch -u AllUsers:R gs://constellation-dag/release/dag-$JAR_TAG.jar
