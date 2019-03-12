b1cdd943d0a7567c106e79df8ee2f0bd57d324ce
ryle-goehausen <ryle.goehausen@gmail.com>
2019-03-12 12:19:08 -0700

    Fix version, needs better fix

--- deploy/compute/assemble-upload.sh
+++ deploy/compute/assemble-upload.sh
@@ -4,5 +4,5 @@ JAR_TAG=${1:-dev}
 
 # Fix version, no version.sbt available anymore
 ./assemble.sh && \
-gsutil cp target/scala-2.12/constellation-assembly-1.0.10.jar gs://constellation-dag/release/dag-$JAR_TAG.jar && \
+gsutil cp target/scala-2.12/constellation-assembly-1.0.12.jar gs://constellation-dag/release/dag-$JAR_TAG.jar && \
 gsutil acl ch -u AllUsers:R gs://constellation-dag/release/dag-$JAR_TAG.jar
