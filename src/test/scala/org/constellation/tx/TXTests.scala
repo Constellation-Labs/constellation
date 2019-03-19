a468f1d3efac7042ce260de5f60e54e3dc82b269
ryle-goehausen <ryle.goehausen@gmail.com>
2019-03-19 14:37:02 -0700

    Fix test formatting

--- src/test/scala/org/constellation/tx/TXTests.scala
+++ src/test/scala/org/constellation/tx/TXTests.scala
@@ -5,6 +5,7 @@ import org.constellation.primitives.ChannelMessage.create
 import org.constellation.primitives._
 import org.scalatest.FlatSpec
 import org.constellation.primitives.Schema.SendToAddress
+import constellation._
 
 class TXTests extends FlatSpec {
 
@@ -25,34 +26,32 @@ class TXTests extends FlatSpec {
 
   "Message distance" should "match threshold" in {
 
-    import constellation._
 
     Seq.tabulate(100) { i =>
       val channelOpenRequest = ChannelOpen(i.toString)
       val genesisMessageStr = channelOpenRequest.json
-      val msg = create(genesisMessageStr, Genesis.CoinBaseHash, channelOpenRequest.name)(Fixtures.kp)
+      val msg =
+        create(genesisMessageStr, Genesis.CoinBaseHash, channelOpenRequest.name)(Fixtures.kp)
       val hash = msg.signedMessageData.hash
       val distance = BigInt(hash.getBytes()) ^ Fixtures.id.bigInt
       distance
     }
+  }
 
-    "Message serialize" should "work with json" in {
+  "Message serialize" should "work with json" in {
 
-      val channelOpenRequest = ChannelOpen("channel", Some(SensorData.jsonSchema))
-      val genesisMessageStr = channelOpenRequest.json
-      val msg = create(genesisMessageStr, Genesis.CoinBaseHash, channelOpenRequest.name)(Fixtures.kp)
-      val hash = msg.signedMessageData.hash
-      val md = ChannelMetadata(channelOpenRequest, ChannelMessageMetadata(msg))
+    val channelOpenRequest = ChannelOpen("channel", Some(SensorData.jsonSchema))
+    val genesisMessageStr = channelOpenRequest.json
+    val msg = create(genesisMessageStr, Genesis.CoinBaseHash, channelOpenRequest.name)(Fixtures.kp)
+    val hash = msg.signedMessageData.hash
+    val md = ChannelMetadata(channelOpenRequest, ChannelMessageMetadata(msg))
 
-      assert(md.json.x[ChannelMetadata] == md)
-
-    }
-
-   // distances.foreach{println}
-
-
-
+    assert(md.json.x[ChannelMetadata] == md)
 
   }
+
+  // distances.foreach{println}
+
+
 
 }
