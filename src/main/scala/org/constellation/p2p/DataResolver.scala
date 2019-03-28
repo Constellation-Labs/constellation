27bdf4c1118e40da43a46428145a331b6d18d1ec
Wojciech Koszycki <koszycki.wojciech@gmail.com>
2019-03-28 14:55:46 +0100

    Unit test for DataResolution

--- src/main/scala/org/constellation/p2p/DataResolver.scala
+++ src/main/scala/org/constellation/p2p/DataResolver.scala
@@ -5,15 +5,15 @@ import constellation._
 import org.constellation.DAO
 import org.constellation.primitives.Schema.CheckpointCacheData
 import org.constellation.primitives.{ChannelMessageMetadata, ChannelProof, TransactionCacheData}
-import org.constellation.util.APIClient
+import org.constellation.util.PeerApiClient
 
 import scala.concurrent.duration._
 
 object DataResolver {
 
   def resolveMessages(hash: String,
-                      pool: Iterable[APIClient],
-                      priorityClient: Option[APIClient] = None)(
+                      pool: Iterable[PeerApiClient],
+                      priorityClient: Option[PeerApiClient] = None)(
     implicit apiTimeout: Duration = 3.seconds,
     dao: DAO
   ): IO[Option[ChannelMessageMetadata]] = {
@@ -31,8 +31,8 @@ object DataResolver {
   }
 
   def resolveTransactions(hash: String,
-                          pool: Iterable[APIClient],
-                          priorityClient: Option[APIClient] = None)(
+                          pool: Iterable[PeerApiClient],
+                          priorityClient: Option[PeerApiClient] = None)(
     implicit apiTimeout: Duration = 3.seconds,
     dao: DAO
   ): IO[Option[TransactionCacheData]] = {
@@ -47,8 +47,8 @@ object DataResolver {
     ).head
   }
   def resolveCheckpoint(hash: String,
-                        pool: Iterable[APIClient],
-                        priorityClient: Option[APIClient] = None)(
+                        pool: Iterable[PeerApiClient],
+                        priorityClient: Option[PeerApiClient] = None)(
     implicit apiTimeout: Duration = 3.seconds,
     dao: DAO
   ): IO[Option[CheckpointCacheData]] = {
@@ -60,12 +60,26 @@ object DataResolver {
                                                },
                                                priorityClient).head
   }
+  def resolveCheckpoints(hashes: List[String],
+                        pool: Iterable[PeerApiClient],
+                        priorityClient: Option[PeerApiClient] = None)(
+    implicit apiTimeout: Duration = 3.seconds,
+    dao: DAO
+  ): IO[List[Option[CheckpointCacheData]]] = {
+    resolveDataByDistanceFlat[CheckpointCacheData](hashes,
+                                               "checkpoint",
+                                               pool,
+                                               (t: CheckpointCacheData) => {
+                                                 t.checkpointBlock.foreach(cb => cb.store(t))
+                                               },
+                                               priorityClient)
+  }
 
   def resolveDataByDistanceFlat[T <: AnyRef](hashes: List[String],
                                              endpoint: String,
-                                             pool: Iterable[APIClient],
+                                             pool: Iterable[PeerApiClient],
                                              store: T => Any,
-                                             priorityClient: Option[APIClient] = None)(
+                                             priorityClient: Option[PeerApiClient] = None)(
     implicit apiTimeout: Duration = 3.seconds,
     m: Manifest[T],
     dao: DAO
@@ -75,9 +89,9 @@ object DataResolver {
 
   def resolveDataByDistance[T <: AnyRef](hashes: List[String],
                                          endpoint: String,
-                                         pool: Iterable[APIClient],
+                                         pool: Iterable[PeerApiClient],
                                          store: T => Any,
-                                         priorityClient: Option[APIClient] = None)(
+                                         priorityClient: Option[PeerApiClient] = None)(
     implicit apiTimeout: Duration = 3.seconds,
     m: Manifest[T],
     dao: DAO
@@ -93,7 +107,7 @@ object DataResolver {
 
   def resolveData[T <: AnyRef](hash: String,
                                endpoint: String,
-                               sortedPeers: Iterable[APIClient],
+                               sortedPeers: Iterable[PeerApiClient],
                                store: T => Any,
                                maxErrors: Int = 10)(
     implicit apiTimeout: Duration = 3.seconds,
@@ -101,24 +115,20 @@ object DataResolver {
     dao: DAO
   ): IO[Option[T]] = {
 
-    def makeAttempt(sortedPeers: Iterable[APIClient], errorsSoFar: Int = 0): IO[Option[T]] = {
+    def makeAttempt(sortedPeers: Iterable[PeerApiClient], errorsSoFar: Int = 0): IO[Option[T]] = {
       sortedPeers match {
         case _ if errorsSoFar >= maxErrors =>
-          IO.raiseError(
-            new RuntimeException(
-              s"Max errors threshold reached when resolving: $endpoint and hash: $hash aborting"
-            )
-          )
+          IO.raiseError(DataResolutionMaxErrors(endpoint, hash))
         case Nil =>
-          IO.raiseError(new RuntimeException(s"Unable to download $endpoint from empty peer list"))
+          IO.raiseError(DataResolutionOutOfPeers(endpoint, sortedPeers.map(_.id.short)))
         case head :: tail =>
           getData[T](hash, endpoint, head, store)
             .handleErrorWith {
               case e if tail.isEmpty => IO.raiseError(e)
-              case _                 => makeAttempt(sortedPeers.tail, errorsSoFar + 1)
+              case _                 => makeAttempt(tail, errorsSoFar + 1)
             }
             .flatMap { response =>
-              if (response.isEmpty) makeAttempt(sortedPeers.tail, errorsSoFar + 1)
+              if (response.isEmpty) makeAttempt(tail, errorsSoFar + 1)
               else IO.pure(response)
             }
       }
@@ -127,7 +137,7 @@ object DataResolver {
   }
   private def getData[T <: AnyRef](hash: String,
                                    endpoint: String,
-                                   client: APIClient,
+                                   client: PeerApiClient,
                                    store: T => Any)(
     implicit apiTimeout: Duration,
     m: Manifest[T],
@@ -144,3 +154,12 @@ object DataResolver {
   }
 
 }
+case class DataResolutionOutOfPeers(endpoint: String, ids: Iterable[String])
+    extends Exception(
+      s"Run out of peers when resolving: $endpoint following tried: $ids"
+    )
+
+case class DataResolutionMaxErrors(endpoint: String, hash: String)
+    extends Exception(
+      s"Max errors threshold reached when resolving: $endpoint and hash: $hash aborting"
+    )
