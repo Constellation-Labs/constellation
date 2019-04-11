f75280a26991eae75566462a5255d6d1f84a983b
buckysballs <wlmeldmanfloch@me.com>
2019-04-10 18:33:22 -0700

    add distribution

--- src/main/scala/org/constellation/consensus/Rewards.scala
+++ src/main/scala/org/constellation/consensus/Rewards.scala
@@ -8,6 +8,15 @@ object Rewards {
   val partitonChart = Map[String, Set[String]]
   val transitiveReputationMatrix = Map[String, Map[String, Double]]
   val neighborhoodReputationMatrix = Map[String, Double]
+  val rewardsPool = 1752000//snapshots - 10yrs @ ~20/hr
+
+  def distribution(curShapshot: Int) = curShapshot match {
+    case num if (num >= 0 && num < 438000)  => 0.68493150684//10k/mo, 328.767123287/day
+    case num if (num >= 438000 && num < 876000) => 0.34246575342
+    case num if (num >= 876000 && num < 1314000) => 0.17123287671
+    case num if (num >= 1314000 && num < 1752000) => 0.08561643835
+    case _ => 0d
+  }
 
   def shannonEntropy(
                       transitiveReputationMatrix: Map[String, Map[String, Double]],
