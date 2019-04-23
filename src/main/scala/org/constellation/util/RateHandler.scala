eb6a8a2f5219fcfae83d0b3db13bbc59c1f433ab
buckysballs <wlmeldmanfloch@me.com>
2019-04-22 23:00:21 -0700

    mock for rate limiting logic

---
+++ src/main/scala/org/constellation/util/RateHandler.scala
@@ -0,0 +1,28 @@
+package org.constellation.util
+import org.constellation.util.Rewards.{rewardDistribution, shannonEntropy}
+
+class RateHandler {
+
+  case class DummyFeeTx(id: String, ammt: Double, fee: Option[Double])
+
+  val totalThroughput = 1000D
+  val offlineTxThroughput = 0.5
+  val validatorThroughput: Double = (1 - offlineTxThroughput) * totalThroughput
+
+  val txSort: Ordering[DummyFeeTx] = Ordering.by(e => (remainingAllowance(e), e.fee))
+
+  /*
+  Service keeping track of submitted tx
+   */
+  def remainingAllowance(id: DummyFeeTx): Int = 0
+
+  def validatorRateAllowance(
+                        transitiveReputationMatrix: Map[String, Map[String, Double]],
+                        neighborhoodReputationMatrix: Map[String, Double],
+                        partitonChart: Map[String, Set[String]]
+                      )= {
+    val trustEntropyMap = shannonEntropy(transitiveReputationMatrix, neighborhoodReputationMatrix)
+    val distro = rewardDistribution(partitonChart, trustEntropyMap)
+    distro.mapValues(_ * validatorThroughput)
+  }
+}
