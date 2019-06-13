3c7eff68d1dade6963175925e0d0884c540239de
Nikolaj-K <Amoogle@hotmail.com>
2019-06-13 22:11:58 +0200

    refine

--- src/main/scala/org/constellation/util/Rewards.scala
+++ src/main/scala/org/constellation/util/Rewards.scala
@@ -3,22 +3,26 @@ package org.constellation.util
 object Rewards {
   val roundingError = 0.000000000001
 
-  // Snapshot epochs. 
-  val epochOne = 438000 // Six months have roughly 4380 hours
-  val epochTwo = 876000 // epochOne * 2
-  val epochThree = 1314000 // epochThree * 2
-  val epochFour = 1752000 // epochFour * 2
-
   /*
-  10k per month or about 328.767123287 per day or about 13.698630136958334 per hour, at ~20 snapshots per hour
+  Set for a first epoch of 2.5 years. 
+  Computed for ~20 snapshots per hour, i.e. about ~14,600 snapshots per month.
    */
-  val epochOneRewards = 0.68493150684 // 13.698630136958334 / 20 or 50 / 73
-  val epochTwoRewards = 0.34246575342 // epochOneRewards / 2
-  val epochThreeRewards = 0.17123287671 // epochTwoRewards / 2
-  val epochFourRewards = 0.08561643835 // epochThreeRewards / 2
+  val epochOne = 438000 // = 14,600 * 12 * 2.5 (2.5 years)
+  val epochTwo = 876000 // = epochOne * 2 (5 years)
+  val epochThree = 1314000 // = epochOne * 3 (7.5 years)
+  val epochFour = 1752000 // = epochOne * 4 (10 years)
 
   /*
-  Partitioning of address space, light nodes have smaller basis that full. Normalizes rewards based on node size.
+  10,000 units per month.
+   */
+  val epochOneRewards = 0.68493150684 // = 10,000 / 14,600
+  val epochTwoRewards = 0.34246575342 // = epochOneRewards / 2
+  val epochThreeRewards = 0.17123287671 // = epochTwoRewards / 2
+  val epochFourRewards = 0.08561643835 // = epochThreeRewards / 2
+
+  /*
+  Partitioning of address space, light nodes have smaller basis that full. 
+  Normalizes rewards based on node size.
    */
   val partitonChart = Map[String, Set[String]]()
 
@@ -27,11 +31,8 @@ object Rewards {
    */
   val transitiveReputationMatrix = Map[String, Map[String, Double]]()
   val neighborhoodReputationMatrix = Map[String, Double]()
-
-  /*
-  Snapshots: ~20 snapshots per hour for 10 years.
-   */
-  val rewardsPool = 1752000 // equals epochFour
+  
+  val rewardsPool = epochFour
 
   def validatorRewards(
                         curShapshot: Int,
@@ -73,6 +74,6 @@ object Rewards {
       (address, reward)
     }
     val totalEntropy = weightedEntropy.values.sum
-    weightedEntropy.mapValues(_ / totalEntropy )// Scale by entropy magnitude
+    weightedEntropy.mapValues(_ / totalEntropy) // Scale by entropy magnitude
   }
 }
