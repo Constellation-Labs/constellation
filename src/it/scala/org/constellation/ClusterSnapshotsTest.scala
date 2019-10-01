c63dd13b2af41c31c5b30dbdb06dfa42ad815183
Marcin Wadoń <mwadon@evojam.com>
2019-10-01 15:25:58 +0200

    ClusterSnapshotTest

--- src/it/scala/org/constellation/ClusterSnapshotsTest.scala
+++ src/it/scala/org/constellation/ClusterSnapshotsTest.scala
@@ -1,3 +1,50 @@
-package org.constellation class ClusterSnapshotsTest {
+package org.constellation
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.testkit.TestKit
+import cats.effect.{ContextShift, IO}
+import cats.implicits._
+import com.softwaremill.sttp.SttpBackend
+import com.softwaremill.sttp.okhttp.OkHttpFutureBackend
+import com.softwaremill.sttp.prometheus.PrometheusBackend
+import org.constellation.storage.RecentSnapshot
+import org.constellation.util.HostPort
+import org.scalatest.{BeforeAndAfterAll, FreeSpec, FreeSpecLike}
+
+import scala.concurrent.Future
+
+class ClusterSnapshotsTest
+    extends TestKit(ActorSystem("ClusterSnapshotsTest"))
+    with FreeSpecLike
+    with BeforeAndAfterAll {
+
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  implicit val materialize: ActorMaterializer = ActorMaterializer()
+  implicit val C: ContextShift[IO] = IO.contextShift(ConstellationExecutionContext.bounded)
+
+  implicit val backend: SttpBackend[Future, Nothing] =
+    OkHttpFutureBackend()(ConstellationExecutionContext.unbounded)
+
+  "should check cluster snapshots heights" in {
+    val (ignoreIPs, auxAPIs) = ComputeTestUtil.getAuxiliaryNodes()
+    val apis = ComputeTestUtil.createApisFromIpFile(ignoreIPs).toList
+
+    val response = apis
+      .traverse(api => api.getNonBlockingIO[List[RecentSnapshot]]("snapshot/recent")(C).map((api.hostName, _)))
+      .unsafeRunSync
+
+    val mapped = response
+      .flatMap(a => a._2.map(s => (s.height -> (s.hash -> a._1))))
+      .groupBy(_._1)
+      .mapValues(_.map(_._2).groupBy(_._1).mapValues(_.map(_._2).size))
+      .toList
+      .sortBy(_._1)
+
+    println(mapped)
+
+  }
 }
