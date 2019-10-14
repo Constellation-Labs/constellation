e723020b4c98b68880420376329e42bac94796dd
mchrapek <mchrapek@evojam.com>
2019-10-14 08:33:40 +0200

    Custom image - filebeat restart

--- deploy/compute/terraform-custom-image/restart.sh
+++ deploy/compute/terraform-custom-image/restart.sh
@@ -5,7 +5,7 @@ HOSTS_FILE=${1:-hosts-dev.txt}
 echo "Restarting $HOSTS_FILE"
 cat ${HOSTS_FILE}
 
-pssh -h ${HOSTS_FILE} -O StrictHostKeyChecking=no -i 'sudo rm -rf /home/ubuntu/constellation/tmp && sudo systemctl restart constellation'
+pssh -h ${HOSTS_FILE} -O StrictHostKeyChecking=no -i 'sudo rm -rf /home/ubuntu/constellation/tmp && sudo systemctl restart constellation && sudo service filebeat restart'
 echo "Done restarting"
 
 export HOSTS_FILE=${HOSTS_FILE}
