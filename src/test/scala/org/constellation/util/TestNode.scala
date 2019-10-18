cd4741077e7312221d49c346058f82a16736c9fa
Wojciech Koszycki <koszycki.wojciech@gmail.com>
2019-10-18 11:33:47 +0200

    Remove duplicated config key nodeConfig.metricIntervalSeconds

--- src/test/scala/org/constellation/util/TestNode.scala
+++ src/test/scala/org/constellation/util/TestNode.scala
@@ -37,7 +37,6 @@ object TestNode {
     val config = NodeConfig(
       seeds = seedHosts,
       primaryKeyPair = keyPair,
-      metricIntervalSeconds = 10, // TODO: wkoszycki duplicate from processingConfig
       isGenesisNode = isGenesisNode,
       isLightNode = isLightNode,
       httpPort = randomPort,
