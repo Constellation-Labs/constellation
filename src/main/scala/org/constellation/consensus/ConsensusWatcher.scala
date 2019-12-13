a584d0fefc78c40e35e1464dddf145191e291e16
Marcin Wadoń <mwadon@evojam.com>
2019-12-13 13:29:41 +0100

    Get rid of implicit Slf4j logger

--- src/main/scala/org/constellation/consensus/ConsensusWatcher.scala
+++ src/main/scala/org/constellation/consensus/ConsensusWatcher.scala
@@ -2,15 +2,12 @@ package org.constellation.consensus
 
 import cats.effect.IO
 import com.typesafe.config.Config
-import com.typesafe.scalalogging.StrictLogging
 import org.constellation.ConfigUtil
 import org.constellation.util.PeriodicIO
 
 import scala.concurrent.duration._
 
-class ConsensusWatcher(config: Config, consensusManager: ConsensusManager[IO])
-    extends PeriodicIO("ConsensusWatcher")
-    with StrictLogging {
+class ConsensusWatcher(config: Config, consensusManager: ConsensusManager[IO]) extends PeriodicIO("ConsensusWatcher") {
 
   override def trigger(): IO[Unit] =
     consensusManager.cleanUpLongRunningConsensus
