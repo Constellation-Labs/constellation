4962bfbbbb38ee72152dd074b4d247d70bb444b4
buckysballs <wlmeldmanfloch@me.com>
2020-01-13 09:09:55 -0800

    comment out writeSnapshotInfoToDisk to prevent kryo buffer issues while testing re-download

--- src/main/scala/org/constellation/storage/SnapshotService.scala
+++ src/main/scala/org/constellation/storage/SnapshotService.scala
@@ -115,7 +115,7 @@ class SnapshotService[F[_]: Concurrent](
             .use(
               stream =>
                 Sync[F].delay {
-                  stream.write(KryoSerializer.serializeAnyRef(info))
+//                  stream.write(KryoSerializer.serializeAnyRef(info))todo save in part files using chunked serialization
                 }.flatTap { _ =>
                   logger.debug(s"SnapshotInfo written for hash: ${info.snapshot.hash} in path: ${path}")
                 }
