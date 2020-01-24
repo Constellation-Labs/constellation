e41d194a4a590057d3962b2a9e0a334de9f5c86d
Krystian Pudlik <kpudlik@evojam.com>
2020-01-24 18:07:33 +0100

    Normalize csv

--- src/test/scala/org/constellation/util/AccountBalanceCSVReaderTest.scala
+++ src/test/scala/org/constellation/util/AccountBalanceCSVReaderTest.scala
@@ -5,15 +5,17 @@ import org.scalatest.Matchers._
 
 class AccountBalanceCSVReaderTest extends FunSuite {
 
-  test("Load CSV file with account balances") {
-    val values = new AccountBalanceCSVReader("src/test/resources/test.csv").read()
+  val src = getClass.getResource("/test.csv")
+
+  test("Load CSV file with account balances and normalize") {
+    val values = new AccountBalanceCSVReader(src.getPath).read()
 
     values.size shouldBe 2
 
     values.head.accountHash shouldBe "abcd"
-    values.head.balance shouldBe 1000L
+    values.head.balance shouldBe 12345678L
 
     values.last.accountHash shouldBe "efgh"
-    values.last.balance shouldBe 2000L
+    values.last.balance shouldBe 187654321L
   }
 }
