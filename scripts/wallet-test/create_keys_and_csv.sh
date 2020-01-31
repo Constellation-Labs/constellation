c3fc84dd3ca0b2df967366a2e18f8a9250aacdfe
Marek Chrapek <marek.chrapek@swing.dev>
2020-01-31 07:54:47 +0100

    704 - Create txs by wallet script

---
+++ scripts/wallet-test/create_keys_and_csv.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+NUMBER_OF_WALLETS=$1
+WORKING_DIR=data
+FILENAME=data.csv
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+USAGE="Proper usage of script:
+  $(basename "$0") number-of-wallets"
+
+if [ -z "${NUMBER_OF_WALLETS}" ]; then
+  echo -e "${RED}Number of wallets is unset $NC";
+  echo "$USAGE";
+  exit 1;
+else
+  echo -e "${GREEN}Number of wallets is set to = $NUMBER_OF_WALLETS $NC";
+fi
+
+if [ -d "$WORKING_DIR" ]; then
+  echo -e "\nRemoving data folder : ${WORKING_DIR}"
+  rm -rf $WORKING_DIR;
+fi
+
+echo -e "Creating data folder : ${WORKING_DIR} \n"
+mkdir -p $WORKING_DIR
+
+for (( i=0; i<NUMBER_OF_WALLETS; i++ ))
+do
+  java -jar cl-keytool.jar --keystore "data/key-${i}.p12" --alias alias --storepass storepass --keypass keypass
+  address=$(java -jar cl-wallet.jar show-address --keystore "data/key-${i}.p12" --alias alias --storepass storepass --keypass keypass)
+  echo -e "Created new key with address : $GREEN $address $NC"
+  echo "${address},1000" >> ${WORKING_DIR}/${FILENAME}
+done
