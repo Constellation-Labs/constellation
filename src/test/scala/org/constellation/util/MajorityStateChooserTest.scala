32e03bf57aaea586d8cdcb0d468277f2f8bf9ec9
Marcin Wadoń <mwadon@evojam.com>
2020-02-05 17:06:27 +0100

    MajorityStateChooser tests temp

--- src/test/scala/org/constellation/util/MajorityStateChooserTest.scala
+++ src/test/scala/org/constellation/util/MajorityStateChooserTest.scala
@@ -8,6 +8,83 @@ import org.constellation.schema.Id
 import org.constellation.storage.RecentSnapshot
 import org.mockito.ArgumentMatchersSugar
 import org.scalatest.{FunSpecLike, Matchers}
+import org.scalatest.FreeSpec
+
+class MajorityStateChooserFreeTest extends FreeSpec with Matchers {
+
+  implicit val contextShift: ContextShift[IO] = IO.contextShift(ConstellationExecutionContext.bounded)
+  implicit val logger: Logger[IO] = Slf4jLogger.getLogger[IO]
+
+  val majorityState = new MajorityStateChooser[IO]
+
+  def toRecentSnapshot(i: Int) = RecentSnapshot(i.toString, i, Map.empty)
+  def toRecentSnapshotWithPrefix(prefix: String)(i: Int) = RecentSnapshot(s"$prefix$i", i, Map.empty)
+
+  "choose majority state" - {
+    "when node creates a correct snapshot" - {
+      "chooses own node if majority at given height is equal" in {
+        val node1 = Id("node1") -> List(0, 2, 4, 6).map(toRecentSnapshot)
+        val node2 = Id("node2") -> List(0, 2, 4, 6).map(toRecentSnapshot)
+
+        val ownNode = Id("ownNode") -> List(0, 2).map(toRecentSnapshot)
+
+        val height = 2L
+
+        val result = majorityState.chooseMajorityState(List(node1, node2, ownNode), height).value.unsafeRunSync.get
+
+        result._1 shouldBe List(2, 0).map(toRecentSnapshot)
+      }
+    }
+
+    "when node creates an incorrect snapshot" - {
+      "chooses all nodes who has the majority at given height if all of them have same amount of snapshots" in {
+        val node1 = Id("node1") -> List(0, 2, 4, 6, 8).map(toRecentSnapshot)
+        val node2 = Id("node2") -> List(0, 2, 4, 6, 8).map(toRecentSnapshot)
+        val node3 = Id("node3") -> (List(0, 2, 4).map(toRecentSnapshot) ++ List(6, 8).map(toRecentSnapshotWithPrefix("a")))
+
+        val ownNode = Id("ownNode") -> (List(0, 2, 4).map(toRecentSnapshot) ++ List(6).map(toRecentSnapshotWithPrefix("b")))
+
+        val height = 6L
+
+        val result = majorityState.chooseMajorityState(List(node1, node2, node3, ownNode), height).value.unsafeRunSync.get
+
+        result._1 shouldBe List(8, 6, 4, 2, 0).map(toRecentSnapshot)
+        result._2 shouldBe List(Id("node1"), Id("node2"))
+      }
+
+      "chooses the node with most snapshots who has the majority at given height" in {
+        val node1 = Id("node1") -> List(0, 2, 4, 6, 8, 10).map(toRecentSnapshot)
+        val node2 = Id("node2") -> List(0, 2, 4, 6, 8).map(toRecentSnapshot)
+        val node3 = Id("node3") -> (List(0, 2, 4).map(toRecentSnapshot) ++ List(6, 8).map(toRecentSnapshotWithPrefix("a")))
+
+        val ownNode = Id("ownNode") -> (List(0, 2, 4).map(toRecentSnapshot) ++ List(6).map(toRecentSnapshotWithPrefix("b")))
+
+        val height = 6L
+
+        val result = majorityState.chooseMajorityState(List(node1, node2, node3, ownNode), height).value.unsafeRunSync.get
+
+        result._1 shouldBe List(10, 8, 6, 4, 2, 0).map(toRecentSnapshot)
+        result._2 shouldBe List(Id("node1"))
+      }
+
+      "chooses the node with most snapshots and the highest snapshot who has the majority at given height" in {
+        val node1 = Id("node1") -> List(2, 4, 6, 8, 10).map(toRecentSnapshot)
+        val node2 = Id("node2") -> List(0, 2, 4, 6, 8).map(toRecentSnapshot)
+        val node3 = Id("node3") -> (List(0, 2, 4).map(toRecentSnapshot) ++ List(6, 8).map(toRecentSnapshotWithPrefix("a")))
+
+        val ownNode = Id("ownNode") -> (List(0, 2, 4).map(toRecentSnapshot) ++ List(6).map(toRecentSnapshotWithPrefix("b")))
+
+        val height = 6L
+
+        val result = majorityState.chooseMajorityState(List(node1, node2, node3, ownNode), height).value.unsafeRunSync.get
+
+        result._1 shouldBe List(10, 8, 6, 4, 2).map(toRecentSnapshot)
+        result._2 shouldBe List(Id("node1"))
+      }
+    }
+  }
+
+}
 
 class MajorityStateChooserTest extends FunSpecLike with ArgumentMatchersSugar with Matchers {
 
