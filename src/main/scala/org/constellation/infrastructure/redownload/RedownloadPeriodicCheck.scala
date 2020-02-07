eb2f23ea0ecb9197455f673727548aee6710e97c
Marcin Wadoń <mwadon@evojam.com>
2020-02-07 17:48:52 +0100

    Periodic check for redownload service

---
+++ src/main/scala/org/constellation/infrastructure/redownload/RedownloadPeriodicCheck.scala
@@ -0,0 +1,27 @@
+package org.constellation.infrastructure.redownload
+
+import cats.effect.{ContextShift, IO}
+import cats.implicits._
+import org.constellation.util.Logging.logThread
+import org.constellation.{ConstellationExecutionContext, DAO}
+import org.constellation.util.PeriodicIO
+
+import scala.concurrent.duration._
+
+class RedownloadPeriodicCheck(periodSeconds: Int = 30)(implicit dao: DAO)
+    extends PeriodicIO("RedownloadPeriodicCheck") {
+
+  val contextShift: ContextShift[IO] = IO.contextShift(ConstellationExecutionContext.bounded)
+
+  private def triggerRedownloadCheck(): IO[Unit] =
+    for {
+      _ <- dao.redownloadService.fetchPeersProposals()
+      _ <- dao.redownloadService.recalculateMajoritySnapshot()
+      _ <- dao.redownloadService.checkForAlignmentWithMajoritySnapshot()
+    } yield ()
+
+  override def trigger(): IO[Unit] = logThread(triggerRedownloadCheck(), "triggerRedownloadCheck", logger)
+
+  schedule(periodSeconds seconds)
+
+}
