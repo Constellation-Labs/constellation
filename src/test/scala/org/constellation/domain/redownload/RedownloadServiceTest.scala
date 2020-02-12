4afa63858ca41e2a6d7a73ce764b8326d6e119b5
Marcin Wadoń <mwadon@evojam.com>
2020-02-12 12:19:15 +0100

    RedownloadService - fetch peers

--- src/test/scala/org/constellation/domain/redownload/RedownloadServiceTest.scala
+++ src/test/scala/org/constellation/domain/redownload/RedownloadServiceTest.scala
@@ -3,16 +3,33 @@ package org.constellation.domain.redownload
 import cats.effect.{ContextShift, IO}
 import cats.implicits._
 import org.constellation.ConstellationExecutionContext
-import org.scalatest.{FreeSpec, Matchers}
+import org.constellation.p2p.{Cluster, PeerData}
+import org.constellation.schema.Id
+import org.constellation.util.APIClient
+import org.mockito.{ArgumentMatchersSugar, IdiomaticMockito}
+import org.mockito.cats.IdiomaticMockitoCats
+import org.scalatest.{BeforeAndAfterEach, FreeSpec, Matchers}
 
-class RedownloadServiceTest extends FreeSpec with Matchers {
+class RedownloadServiceTest
+    extends FreeSpec
+    with Matchers
+    with IdiomaticMockito
+    with IdiomaticMockitoCats
+    with BeforeAndAfterEach
+    with ArgumentMatchersSugar {
 
   implicit val cs: ContextShift[IO] = IO.contextShift(ConstellationExecutionContext.unbounded)
 
+  var cluster: Cluster[IO] = _
+  var redownloadService: RedownloadService[IO] = _
+
+  override def beforeEach() = {
+    cluster = mock[Cluster[IO]]
+    redownloadService = RedownloadService[IO](cluster)
+  }
+
   "persistOwnSnapshot" - {
     "should persist own snapshot internally if snapshot at given height doesn't exist" in {
-      val redownloadService = RedownloadService[IO]()
-
       val persist = redownloadService.persistOwnSnapshot(2L, "aabbcc")
       val check = redownloadService.ownSnapshots.get.map(_.get(2L))
 
@@ -20,8 +37,6 @@ class RedownloadServiceTest extends FreeSpec with Matchers {
     }
 
     "should not override previously persisted snapshot if snapshot at given height already exists" in {
-      val redownloadService = RedownloadService[IO]()
-
       val persistFirst = redownloadService.persistOwnSnapshot(2L, "aaaa")
       val persistSecond = redownloadService.persistOwnSnapshot(2L, "bbbb")
       val check = redownloadService.ownSnapshots.get.map(_.get(2L))
@@ -32,16 +47,12 @@ class RedownloadServiceTest extends FreeSpec with Matchers {
 
   "getOwnSnapshots" - {
     "should return empty map if there are no own snapshots" in {
-      val redownloadService = RedownloadService[IO]()
-
       val check = redownloadService.getOwnSnapshots()
 
       check.unsafeRunSync shouldBe Map.empty
     }
 
     "should return all own snapshots if they exist" in {
-      val redownloadService = RedownloadService[IO]()
-
       val persistFirst = redownloadService.persistOwnSnapshot(2L, "aaaa")
       val persistSecond = redownloadService.persistOwnSnapshot(4L, "bbbb")
       val check = redownloadService.getOwnSnapshots()
@@ -52,8 +63,6 @@ class RedownloadServiceTest extends FreeSpec with Matchers {
 
   "getOwnSnapshot" - {
     "should return hash if snapshot at given height exists" in {
-      val redownloadService = RedownloadService[IO]()
-
       val persist = redownloadService.persistOwnSnapshot(2L, "aaaa")
       val check = redownloadService.getOwnSnapshot(2L)
 
@@ -61,11 +70,88 @@ class RedownloadServiceTest extends FreeSpec with Matchers {
     }
 
     "should return None if snapshot at given height does not exist" in {
-      val redownloadService = RedownloadService[IO]()
-
       val check = redownloadService.getOwnSnapshot(2L)
 
       check.unsafeRunSync shouldBe none[String]
+    }
+  }
+
+  "fetchPeersProposals" - {
+    "should fetch own proposals of all the peers" in {
+      val peerInfo = Map(Id("node1") -> mock[PeerData], Id("node2") -> mock[PeerData])
+      peerInfo.values.foreach { peer =>
+        peer.client shouldReturn mock[APIClient]
+        peer.client.getNonBlockingF[IO, Map[Long, String]](*, *, *)(*)(*, *, *) shouldReturnF Map.empty
+      }
+
+      cluster.getPeerInfo shouldReturnF peerInfo
+
+      redownloadService.fetchPeersProposals().unsafeRunSync
+
+      peerInfo.values.foreach { peer =>
+        peer.client.getNonBlockingF[IO, Map[Long, String]]("/snapshot/own", *, *)(*)(*, *, *).was(called)
+      }
+    }
+
+    "should modify the local peers proposals store" in {
+      val peer1 = mock[PeerData]
+      val peer2 = mock[PeerData]
+
+      val peerInfo = Map(Id("node1") -> peer1, Id("node2") -> peer2)
+      val proposals = Map(2L -> "aa", 4L -> "bb")
+
+      val ids = List(Id("node1"), Id("node2"))
+
+      peerInfo.values.zip(ids).foreach {
+        case (peer, id) =>
+          peer.client shouldReturn mock[APIClient]
+          peer.client.id shouldReturn id
+          peer.client.getNonBlockingF[IO, Map[Long, String]](*, *, *)(*)(*, *, *) shouldReturnF proposals
+      }
+
+      cluster.getPeerInfo shouldReturnF peerInfo
+
+      val fetch = redownloadService.fetchPeersProposals()
+      val check = redownloadService.peersProposals.get
+
+      (fetch >> check).unsafeRunSync shouldBe Map(
+        Id("node1") -> proposals,
+        Id("node2") -> proposals
+      )
+    }
+
+    /**
+      * TODO: Consider as a feature.
+      * If proposals are immutable, it can be a sanity check that nodes are not changing the proposals.
+      */
+    "should not override previously stored proposals" ignore {}
+
+    "should not fail if at least one peer did not respond" in {
+      val peer1 = mock[PeerData]
+      val peer2 = mock[PeerData]
+
+      val peerInfo = Map(Id("node1") -> peer1, Id("node2") -> peer2)
+      val proposals = Map(2L -> "aa", 4L -> "bb")
+
+      peer1.client shouldReturn mock[APIClient]
+      peer1.client.id shouldReturn Id("node1")
+      peer1.client.getNonBlockingF[IO, Map[Long, String]](*, *, *)(*)(*, *, *) shouldReturnF proposals
+
+      peer2.client shouldReturn mock[APIClient]
+      peer2.client.id shouldReturn Id("node2")
+      peer2.client.getNonBlockingF[IO, Map[Long, String]](*, *, *)(*)(*, *, *) shouldReturn IO.raiseError(
+        new Throwable("error")
+      )
+
+      cluster.getPeerInfo shouldReturnF peerInfo
+
+      val fetch = redownloadService.fetchPeersProposals()
+      val check = redownloadService.peersProposals.get
+
+      (fetch >> check).unsafeRunSync shouldBe Map(
+        Id("node1") -> proposals,
+        Id("node2") -> Map.empty
+      )
     }
   }
 
