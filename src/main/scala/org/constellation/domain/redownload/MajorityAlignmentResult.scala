0a2ac6c612961d9e989b8bbc8186cb3ae964ce2e
Krystian Pudlik <kpudlik@evojam.com>
2020-02-17 16:05:59 +0100

    Unit tested implementation of "shouldRedownload"

--- src/main/scala/org/constellation/domain/redownload/MajorityAlignmentResult.scala
+++ src/main/scala/org/constellation/domain/redownload/MajorityAlignmentResult.scala
@@ -3,6 +3,11 @@ package org.constellation.domain.redownload
 sealed trait MajorityAlignmentResult
 
 object AlignedWithMajority extends MajorityAlignmentResult
-object BelowMaxMajorityHeight extends MajorityAlignmentResult
-object AboveMaxMajorityHeight extends MajorityAlignmentResult
-object MisalignedWithMajority extends MajorityAlignmentResult
+
+object MisalignedAtSameHeight extends MajorityAlignmentResult
+
+object BelowPastRedownloadInterval extends MajorityAlignmentResult
+object BelowButMisalignedBeforeRedownloadIntervalAnyway extends MajorityAlignmentResult
+
+object AbovePastRedownloadInterval extends MajorityAlignmentResult
+object AboveButMisalignedBeforeRedownloadIntervalAnyway extends MajorityAlignmentResult
