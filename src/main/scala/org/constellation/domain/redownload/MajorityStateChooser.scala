a5bf798420cacd37b668360aba03cd633aca2870
Marcin Wadoń <mwadon@evojam.com>
2020-02-19 13:44:25 +0100

    Majority state uses the most quantity as fallback

--- src/main/scala/org/constellation/domain/redownload/MajorityStateChooser.scala
+++ src/main/scala/org/constellation/domain/redownload/MajorityStateChooser.scala
@@ -3,52 +3,62 @@ package org.constellation.domain.redownload
 import cats.effect.Concurrent
 import cats.implicits._
 import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
-import org.constellation.domain.redownload.MajorityStateChooser.{MajorityState, Occurrences}
+import org.constellation.domain.redownload.MajorityStateChooser.Occurrences
+import org.constellation.domain.redownload.RedownloadService.SnapshotsAtHeight
 import org.constellation.schema.Id
 
 class MajorityStateChooser {
 
   // TODO: Use RedownloadService type definitions
   def chooseMajorityState(
-    createdSnapshots: Map[Long, String],
-    peersProposals: Map[Id, Map[Long, String]]
-  ): MajorityState = {
+    createdSnapshots: SnapshotsAtHeight,
+    peersProposals: Map[Id, SnapshotsAtHeight]
+  ): SnapshotsAtHeight = {
     val peersSize = peersProposals.size + 1 // +1 - it's an own node
     val proposals = mergeByHeights(createdSnapshots, peersProposals)
 
     val flat = proposals
       .mapValues(_.sorted)
       .mapValues(mapToOccurrences)
-      .mapValues(_.find(a => isInMajority(a.n, peersSize)))
+      .mapValues { occurrences =>
+        occurrences
+          .find(o => isInClearMajority(o.n, peersSize))
+          .orElse(getTheMostQuantity(occurrences, peersSize))
+      }
       .mapValues(_.map(_.value))
 
     for ((k, Some(v)) <- flat) yield k -> v
   }
 
-  private def isInMajority(occurrences: Int, total: Int): Boolean =
-    if (total > 0)
-      occurrences / total.toDouble >= 0.5
+  private def isInClearMajority(occurrences: Int, totalPeers: Int): Boolean =
+    if (totalPeers > 0)
+      occurrences / totalPeers.toDouble >= 0.5
     else false
+
+  private def getTheMostQuantity[A](occurrences: Set[Occurrences[A]], totalPeers: Int): Option[Occurrences[A]] =
+    if (occurrences.toList.map(_.n).sum == totalPeers) {
+      occurrences.toList.sortBy(-_.percentage).headOption
+    } else None
 
   private def mapValuesToList[K, V](a: Map[K, V]): Map[K, List[V]] =
     a.mapValues(List(_))
 
   private def mergeByHeights(
-    createdSnapshots: Map[Long, String],
-    peersProposals: Map[Id, Map[Long, String]]
+    createdSnapshots: SnapshotsAtHeight,
+    peersProposals: Map[Id, SnapshotsAtHeight]
   ): Map[Long, List[String]] =
     (peersProposals.mapValues(mapValuesToList).values.toList ++ List(mapValuesToList(createdSnapshots)))
       .fold(Map.empty)(_ |+| _)
 
   private def mapToOccurrences(values: List[String]): Set[Occurrences[String]] =
-    values.toSet.map((a: String) => Occurrences(a, values.count(_ == a)))
+    values.toSet.map((a: String) => Occurrences(a, values.count(_ == a), values.size))
 
 }
 
 object MajorityStateChooser {
   def apply(): MajorityStateChooser = new MajorityStateChooser()
 
-  type MajorityState = Map[Long, String]
-
-  case class Occurrences[A](value: A, n: Int)
+  case class Occurrences[A](value: A, n: Int, of: Int) {
+    val percentage: Double = n / of.toDouble
+  }
 }
