8903af338973754f8d4f39af1e2131c1851f61d4
Krystian Pudlik <kpudlik@evojam.com>
2020-04-20 14:03:54 +0200

    Additional logs for JoiningPeerValidator

--- src/main/scala/org/constellation/p2p/JoiningPeerValidator.scala
+++ src/main/scala/org/constellation/p2p/JoiningPeerValidator.scala
@@ -26,7 +26,42 @@ class JoiningPeerValidator[F[_]: Concurrent](apiClient: ClientInterpreter[F]) {
   private def validateBuildInfo(peerClientMetadata: PeerClientMetadata): F[ValidationResult[String]] = {
     val validate: F[ValidationResult[String]] = for {
       peerBuildInfo <- apiClient.buildInfo.getBuildInfo().run(peerClientMetadata)
+      _ <- logger.debug(s"BuildInfo (peer): ${peerBuildInfo}")
+
       buildInfo = BuildInfoJson()
+      _ <- logger.debug(s"BuildInfo (node): ${buildInfo}")
+
+      copiedPeerBuildInfo = BuildInfoJson(
+        name = peerBuildInfo.name,
+        version = peerBuildInfo.version,
+        scalaVersion = peerBuildInfo.scalaVersion,
+        sbtVersion = peerBuildInfo.sbtVersion,
+        gitBranch = peerBuildInfo.gitBranch,
+        gitCommit = peerBuildInfo.gitCommit,
+        builtAtString = peerBuildInfo.builtAtString,
+        builtAtMillis = peerBuildInfo.builtAtMillis
+      )
+
+      _ <- logger.debug(s"BuildInfo copy (peer): ${copiedPeerBuildInfo}")
+
+      copiedBuildInfo = BuildInfoJson(
+        name = buildInfo.name,
+        version = buildInfo.version,
+        scalaVersion = buildInfo.scalaVersion,
+        sbtVersion = buildInfo.sbtVersion,
+        gitBranch = buildInfo.gitBranch,
+        gitCommit = buildInfo.gitCommit,
+        builtAtString = buildInfo.builtAtString,
+        builtAtMillis = buildInfo.builtAtMillis
+      )
+
+      _ <- logger.debug(s"BuildInfo copy (node): ${copiedBuildInfo}")
+
+      copiedBuildInfoSerialized = KryoSerializer.serializeAnyRef(copiedBuildInfo).sha256
+      _ <- logger.debug(s"Node build info (copy) hash=$copiedBuildInfoSerialized")
+
+      copiedPeerBuildInfoSerialized = KryoSerializer.serializeAnyRef(copiedPeerBuildInfo).sha256
+      _ <- logger.debug(s"Joining peer build info (copy) hash=$copiedPeerBuildInfoSerialized")
 
       peerBuildInfoSerialized = KryoSerializer.serializeAnyRef(peerBuildInfo).sha256
       _ <- logger.debug(s"Joining peer build info hash=$peerBuildInfoSerialized")
