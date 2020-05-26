f663a7a77a166f9ab511725275f7a725fa9a1cea
Marcin Wadoń <mwadon@evojam.com>
2020-05-26 17:46:41 +0200

    Propose only peers with ability to act as joining sources and endpoint for owner for majority heights

--- src/main/scala/org/constellation/infrastructure/endpoints/NodeMetadataEndpoints.scala
+++ src/main/scala/org/constellation/infrastructure/endpoints/NodeMetadataEndpoints.scala
@@ -2,10 +2,12 @@ package org.constellation.infrastructure.endpoints
 
 import cats.effect.Concurrent
 import cats.implicits._
+import io.circe.KeyEncoder
 import io.circe.generic.auto._
 import io.circe.syntax._
 import org.constellation.p2p.Cluster
-import org.constellation.primitives.Schema.NodeType
+import org.constellation.primitives.Schema.{NodeState, NodeType}
+import org.constellation.schema.Id
 import org.constellation.storage.AddressService
 import org.constellation.util.NodeStateInfo
 import org.http4s.HttpRoutes
@@ -39,7 +41,25 @@ class NodeMetadataEndpoints[F[_]](implicit F: Concurrent[F]) extends Http4sDsl[F
 
   private def getNodePeers(cluster: Cluster[F]): HttpRoutes[F] = HttpRoutes.of[F] {
     case GET -> Root / "peers" =>
-      cluster.getPeerInfo.map(_.map(_._2.peerMetadata).toSeq).map(_.asJson).flatMap(Ok(_))
+      cluster.getPeerInfo
+        .map(
+          _.map { case (_, pd) => pd }
+            .filter(pd => NodeState.canActAsJoiningSource(pd.peerMetadata.nodeState))
+            .map(_.peerMetadata)
+            .toSeq
+        )
+        .map(_.asJson)
+        .flatMap(Ok(_))
+  }
+
+  implicit val idEncoder: KeyEncoder[Id] = KeyEncoder.encodeKeyString.contramap[Id](_.hex)
+
+  private def getPeersMajorityHeights(cluster: Cluster[F]): HttpRoutes[F] = HttpRoutes.of[F] {
+    case GET -> Root / "peers" / "majority-height" =>
+      cluster.getPeerInfo
+        .map(_.mapValues(_.majorityHeight.toList))
+        .map(_.asJson)
+        .flatMap(Ok(_))
   }
 
   def ownerEndpoints(
@@ -48,7 +68,10 @@ class NodeMetadataEndpoints[F[_]](implicit F: Concurrent[F]) extends Http4sDsl[F
     addresses: Seq[String],
     nodeType: NodeType
   ) =
-    getNodeState(cluster, addresses, nodeType) <+> getAddressBalance(addressService) <+> getNodePeers(cluster)
+    getNodeState(cluster, addresses, nodeType) <+>
+      getAddressBalance(addressService) <+>
+      getNodePeers(cluster) <+>
+      getPeersMajorityHeights(cluster)
 
 }
 
