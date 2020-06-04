88f862260c65749c28fbbcb03aaff730667f5132
Maciej Muszyński <30803273+TheMMaciek@users.noreply.github.com>
2020-06-04 17:38:38 +0200

    Add test generating data with BIP44 for bolos app integration testing

--- src/test/scala/org/constellation/wallet/BIP44Test.scala
+++ src/test/scala/org/constellation/wallet/BIP44Test.scala
@@ -1,8 +1,12 @@
 package org.constellation.wallet
+
 import org.bitcoinj.core.Sha256Hash
+import org.constellation.Fixtures
 import org.constellation.keytool.KeyUtils
 import org.scalatest.{FreeSpec, Matchers}
 import org.web3j.crypto.Hash
+import io.circe.generic.auto._
+import io.circe.syntax._
 
 import scala.util.Try
 
@@ -33,5 +37,46 @@ class BIP44Test extends FreeSpec with Matchers {
       childKeyObj.verify(Sha256Hash.wrap(msg), btcJSignature)
     }
     assert(isBTCValid.isFailure)
+  }
+
+  "Transaction should be signed correctly" in {
+    val childIndex = 0
+    val bip44 = new BIP44(seedCode, childIndex)
+    val keyPair = bip44.getChildKeyPairOfDepth()
+    val publicKey = keyPair.getPublic
+    val privateKey = keyPair.getPrivate
+    val address = KeyUtils.publicKeyToAddressString(publicKey)
+
+    val src = address
+    val dst = "DAG48nmxgpKhZzEzyc86y9oHotxxG57G8sBBwj56"
+    val amount = 100000000L
+    val prevHash = "08e6f0c3d65ed0b393604ffe282374bf501956ba447bc1c5ac49bcd2e8cc44fd"
+    val ordinal = 567L
+    val fee = 123456L
+    val salt = 7370566588033602435L
+    val lastTxRef = LastTransactionRef(prevHash, ordinal)
+    val txData = TransactionEdgeData(amount = amount, lastTxRef = lastTxRef, fee = Option(fee), salt = salt)
+    val oe = ObservationEdge(
+      Seq(
+        TypedEdgeHash(src, EdgeHashType.AddressHash),
+        TypedEdgeHash(dst, EdgeHashType.AddressHash)
+      ),
+      TypedEdgeHash(txData.getEncoding, EdgeHashType.TransactionDataHash)
+    )
+    val runLengthEncoding = oe.getEncoding
+    val soe = SignHelp.signedObservationEdge(oe)(keyPair)
+    val tx = Transaction(edge = Edge(oe, soe, txData), lastTxRef = lastTxRef, isDummy = false, isTest = false)
+
+//    val signature = bip44.signData(tx.edge.observationEdge.hash.getBytes())
+//    val signDataManually = KeyUtils.bytes2hex(signature)
+
+    println("mnemonic: " + seedCode)
+    println("bip44 path: " + bip44.chainPathPrefix + childIndex)
+    println("privateKey: " + KeyUtils.privateKeyToHex(privateKey))
+    println("publicKey: " + KeyUtils.publicKeyToHex(publicKey))
+    println("address: " + address)
+    println("transaction: " + tx.asJson.noSpaces)
+    println("hex encoded signature: " + tx.edge.signedObservationEdge.signatureBatch.signatures.head.signature)
+    println("runLengthEncoding: " + runLengthEncoding)
   }
 }
