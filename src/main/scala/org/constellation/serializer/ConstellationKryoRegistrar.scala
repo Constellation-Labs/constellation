c3d0f06812f10953be80b85fe066fcf9cf32f191
Marcin Wadoń <mwadon@evojam.com>
2020-09-18 13:23:50 +0200

    Register kryo classes

--- src/main/scala/org/constellation/serializer/ConstellationKryoRegistrar.scala
+++ src/main/scala/org/constellation/serializer/ConstellationKryoRegistrar.scala
@@ -30,99 +30,110 @@ import scala.collection.SortedMap
 import scala.collection.immutable.TreeMap
 
 class ConstellationKryoRegistrar extends IKryoRegistrar {
+  private val printRegistrationIDs = false
 
-  override def apply(kryo: Kryo): Unit =
+  override def apply(kryo: Kryo): Unit = {
     this.registerClasses(kryo)
 
+    if (printRegistrationIDs) {
+      val nextId = kryo.getNextRegistrationId
+      println(s"Next ID: $nextId")
+
+      (0 until nextId).foreach { i =>
+        val registration = kryo.getRegistration(i)
+        println(s"ID: ${registration.getId}, ${registration.getType.getName}")
+      }
+    }
+  }
+
   def registerClasses(kryo: Kryo): Unit = {
-    kryo.register(classOf[ChannelMessageData])
-    kryo.register(classOf[SignedData[ChannelMessageData]])
-    kryo.register(classOf[SignedData[ObservationData]])
-    kryo.register(classOf[SignedData[ObservationData]])
-    kryo.register(classOf[ChannelMessage])
-    kryo.register(classOf[Seq[ChannelMessage]])
-    kryo.register(classOf[StoredSnapshot])
-    kryo.register(classOf[SnapshotInfo])
-    kryo.register(classOf[TipData])
-    kryo.register(classOf[Height])
-    kryo.register(classOf[Option[Height]])
-    kryo.register(classOf[CommonMetadata])
-    kryo.register(classOf[Seq[CheckpointBlock]])
-    kryo.register(classOf[Seq[Transaction]])
-    kryo.register(classOf[SignedData[ObservationData]])
-    kryo.register(classOf[Seq[CheckpointCache]])
-    kryo.register(classOf[Map[String, AddressCacheData]])
-    kryo.register(classOf[Map[String, TipData]])
-    kryo.register(classOf[Map[String, LastTransactionRef]])
-    kryo.register(classOf[Map[Id, Double]])
-    kryo.register(classOf[Map[IP, Id]])
-    kryo.register(classOf[SortedMap[Id, Double]])
-    kryo.register(classOf[TreeMap[Id, Double]])
-    kryo.register(classOf[Seq[(String, AddressCacheData)]])
-    kryo.register(classOf[Seq[(String, TipData)]])
-    kryo.register(classOf[Seq[(String, LastTransactionRef)]])
-    kryo.register(classOf[Seq[(Id, Double)]])
-    kryo.register(classOf[Seq[(IP, Id)]])
-    kryo.register(classOf[Address])
-    kryo.register(classOf[CheckpointEdge])
-    kryo.register(classOf[AddressCacheData])
-    kryo.register(classOf[TransactionCacheData])
-    kryo.register(classOf[CheckpointCache])
+    kryo.register(classOf[ChannelMessageData], 145)
+    kryo.register(classOf[SignedData[ChannelMessageData]], 146)
+    kryo.register(classOf[SignedData[ObservationData]]) // todo
+    kryo.register(classOf[SignedData[ObservationData]]) // todo
+    kryo.register(classOf[ChannelMessage], 147)
+    kryo.register(classOf[Seq[ChannelMessage]], 148)
+    kryo.register(classOf[StoredSnapshot], 149)
+    kryo.register(classOf[SnapshotInfo], 150)
+    kryo.register(classOf[TipData], 151)
+    kryo.register(classOf[Height], 152)
+    kryo.register(classOf[Option[Height]], 153)
+    kryo.register(classOf[CommonMetadata], 154)
+    kryo.register(classOf[Seq[CheckpointBlock]]) // todo
+    kryo.register(classOf[Seq[Transaction]]) // todo
+    kryo.register(classOf[SignedData[ObservationData]]) // todo
+    kryo.register(classOf[Seq[CheckpointCache]]) // todo
+    kryo.register(classOf[Map[String, AddressCacheData]], 155)
+    kryo.register(classOf[Map[String, TipData]]) // todo
+    kryo.register(classOf[Map[String, LastTransactionRef]]) // todo
+    kryo.register(classOf[Map[Id, Double]]) // todo
+    kryo.register(classOf[Map[IP, Id]]) // todo
+    kryo.register(classOf[SortedMap[Id, Double]], 156)
+    kryo.register(classOf[TreeMap[Id, Double]]) // todo
+    kryo.register(classOf[Seq[(String, AddressCacheData)]]) // todo
+    kryo.register(classOf[Seq[(String, TipData)]]) // todo
+    kryo.register(classOf[Seq[(String, LastTransactionRef)]]) // todo
+    kryo.register(classOf[Seq[(Id, Double)]]) // todo
+    kryo.register(classOf[Seq[(IP, Id)]]) // todo
+    kryo.register(classOf[Address], 157)
+    kryo.register(classOf[CheckpointEdge], 158)
+    kryo.register(classOf[AddressCacheData], 159)
+    kryo.register(classOf[TransactionCacheData], 160)
+    kryo.register(classOf[CheckpointCache], 161)
     kryo.register(classOf[Transaction], 1001)
-    kryo.register(classOf[TransactionGossip])
+    kryo.register(classOf[TransactionGossip], 162)
     kryo.register(classOf[Edge[TransactionEdgeData]], 1005)
     kryo.register(classOf[Edge[CheckpointEdgeData]])
     kryo.register(classOf[SignatureBatch], 1006)
     kryo.register(classOf[HashSignature], 1007)
     kryo.register(classOf[SignedObservationEdge], 1003)
     kryo.register(classOf[ObservationEdge], 1002)
-    kryo.register(classOf[CheckpointBlock])
-    kryo.register(classOf[PeerNotification])
+    kryo.register(classOf[CheckpointBlock], 163)
+    kryo.register(classOf[PeerNotification], 164)
     kryo.register(classOf[Seq[PeerNotification]])
     kryo.register(classOf[TypedEdgeHash], 1004)
     kryo.register(classOf[Enumeration#Value], 1008)
     kryo.register(classOf[TransactionEdgeData], 1009)
-    kryo.register(classOf[CheckpointEdgeData])
-    kryo.register(classOf[Snapshot])
-    kryo.register(classOf[GenesisObservation])
+    kryo.register(classOf[CheckpointEdgeData], 165)
+    kryo.register(classOf[Snapshot], 166)
+    kryo.register(classOf[GenesisObservation], 167)
     kryo.register(classOf[LastTransactionRef], 1010)
-    kryo.register(classOf[ObservationData])
-    kryo.register(classOf[CheckpointBlockWithMissingParents])
-    kryo.register(classOf[CheckpointBlockWithMissingSoe])
-    kryo.register(classOf[RequestTimeoutOnConsensus])
-    kryo.register(classOf[RequestTimeoutOnResolving])
-    kryo.register(classOf[CheckpointBlockInvalid])
-    kryo.register(classOf[Observation])
-    kryo.register(classOf[Seq[Observation]])
-    kryo.register(classOf[scala.collection.mutable.ArrayBuffer[String]])
-    kryo.register(classOf[Seq[String]])
-    kryo.register(classOf[Set[String]])
-    kryo.register(classOf[Seq[String]])
+    kryo.register(classOf[ObservationData], 168)
+    kryo.register(classOf[CheckpointBlockWithMissingParents], 169)
+    kryo.register(classOf[CheckpointBlockWithMissingSoe], 170)
+    kryo.register(classOf[RequestTimeoutOnConsensus], 171)
+    kryo.register(classOf[RequestTimeoutOnResolving], 172)
+    kryo.register(classOf[CheckpointBlockInvalid], 173)
+    kryo.register(classOf[Observation], 174)
+    kryo.register(classOf[Seq[Observation]]) // todo
+    kryo.register(classOf[scala.collection.mutable.ArrayBuffer[String]], 175)
+    kryo.register(classOf[Seq[String]]) // todo
+    kryo.register(classOf[Set[String]], 176)
 
     kryo.register(classOf[Id], 1011)
 
     kryo.register(classOf[Array[Byte]], 1012)
-    kryo.register(classOf[Array[Array[Byte]]])
+    kryo.register(classOf[Array[Array[Byte]]], 177)
     kryo.register(classOf[Option[Long]], 1013)
     kryo.register(classOf[String], 1014)
     kryo.register(classOf[Boolean], 1015)
 
     // EigenTrustJ
-    kryo.register(classOf[DefaultRandomGenerator])
-    kryo.register(classOf[MersenneTwister])
-    kryo.register(classOf[cern.jet.random.Normal])
-    kryo.register(classOf[cern.jet.random.Uniform])
-    kryo.register(Class.forName("[[I")) // To make int[][] serializable
-    kryo.register(classOf[EigenTrustJ])
-    kryo.register(classOf[EigenTrustAgents])
-    kryo.register(classOf[StoredRewards])
+    kryo.register(classOf[DefaultRandomGenerator], 178)
+    kryo.register(classOf[MersenneTwister], 179)
+    kryo.register(classOf[cern.jet.random.Normal], 180)
+    kryo.register(classOf[cern.jet.random.Uniform], 181)
+    kryo.register(Class.forName("[[I"), 182) // To make int[][] serializable
+    kryo.register(classOf[EigenTrustJ], 183)
+    kryo.register(classOf[EigenTrustAgents], 184)
+    kryo.register(classOf[StoredRewards], 185)
 
-    kryo.register(classOf[AddressMetaData])
-    kryo.register(classOf[BuildInfoJson])
+    kryo.register(classOf[AddressMetaData], 186)
+    kryo.register(classOf[BuildInfoJson], 187)
 
-    kryo.register(Class.forName("scala.math.LowPriorityOrderingImplicits$$anon$3"))
-    kryo.register(Class.forName("scala.Predef$$anon$2"))
-    kryo.register(scala.math.Ordering.String.getClass)
+    kryo.register(Class.forName("scala.math.LowPriorityOrderingImplicits$$anon$3"), 188)
+    kryo.register(Class.forName("scala.Predef$$anon$2"), 189)
+    kryo.register(scala.math.Ordering.String.getClass) // todo
 
     kryo.register(EdgeHashType.AddressHash.getClass, 1024)
     kryo.register(EdgeHashType.CheckpointDataHash.getClass, 1025)
@@ -134,13 +145,13 @@ class ConstellationKryoRegistrar extends IKryoRegistrar {
     kryo.register(EdgeHashType.ChannelMessageDataHash.getClass, 1031)
 
     kryo.register(Class.forName("scala.Enumeration$Val"), 1017)
-    kryo.register(Class.forName("scala.collection.immutable.HashSet$HashSet1"), 1018)
-    kryo.register(Class.forName("scala.collection.immutable.Set$EmptySet$"), 1019)
-    kryo.register(Class.forName("scala.collection.IndexedSeqLike$Elements"))
+    kryo.register(Class.forName("scala.collection.immutable.HashSet$HashSet1"), 135)
+    kryo.register(Class.forName("scala.collection.immutable.Set$EmptySet$"), 131)
+    kryo.register(Class.forName("scala.collection.IndexedSeqLike$Elements"), 190)
     kryo.register(Class.forName("scala.collection.immutable.$colon$colon"), 1020)
-    kryo.register(Class.forName("scala.None$"), 1021)
-    kryo.register(Class.forName("scala.collection.immutable.Nil$"), 1022)
-    kryo.register(Class.forName("scala.collection.immutable.Map$EmptyMap$"), 1023)
+    kryo.register(Class.forName("scala.None$"), 114)
+    kryo.register(Class.forName("scala.collection.immutable.Nil$"), 116)
+    kryo.register(Class.forName("scala.collection.immutable.Map$EmptyMap$"), 136)
 
   }
 }
