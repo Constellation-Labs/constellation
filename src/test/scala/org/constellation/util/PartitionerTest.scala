5885b170fd2529e3a4a66f1ca28ed2a6915f1748
buckysballs <wlmeldmanfloch@me.com>
2020-11-10 12:47:26 -0800

    unit tests and cleanup

--- src/test/scala/org/constellation/util/PartitionerTest.scala
+++ src/test/scala/org/constellation/util/PartitionerTest.scala
@@ -1,15 +1,23 @@
 package org.constellation.util
 
-import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
+import org.constellation.Fixtures
 import org.constellation.Fixtures._
-import Partitioner._
+import org.constellation.domain.trust.TrustDataInternal
 import org.constellation.schema.transaction.Transaction
+import org.constellation.trust.{DataGeneration, TrustNode}
+import org.constellation.util.Partitioner._
 import org.scalatest.flatspec.AsyncFlatSpecLike
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
 
 class PartitionerTest extends AsyncFlatSpecLike with Matchers with BeforeAndAfterAll with BeforeAndAfterEach {
 
   val random = new java.util.Random()
+  val randomTxs = getRandomTxs()
+  val acceptableFacilBalance = 0.8
+  val proposerId = Fixtures.id
+  val ids = proposerId :: idSet5.toList
+  val idxId = ids.zipWithIndex.toMap.map { case (k, v) => (v, k) }
 
   def getRandomTxs(factor: Int = 5): Set[Transaction] = idSet5.flatMap { id =>
     val destinationAddresses = idSet5.map(_.address)
@@ -18,9 +26,6 @@ class PartitionerTest extends AsyncFlatSpecLike with Matchers with BeforeAndAfte
       destStr => makeTransaction(id.address, destStr, random.nextLong(), getRandomElement(tempKeySet, random))
     )
   }
-  val randomTxs = getRandomTxs()
-  val acceptableFacilBalance = 0.8
-  val ids = idSet5.toList
 
   "Facilitator selection" should "be deterministic" in {
     val facilitator = selectTxFacilitator(ids, randomTxs.head)
@@ -35,7 +40,7 @@ class PartitionerTest extends AsyncFlatSpecLike with Matchers with BeforeAndAfte
 
   "Facilitator selection" should "be relatively balanced" in {
     val facilitators = randomTxs.map(tx => selectTxFacilitator(ids, tx))
-    assert(facilitators.size == 5)
+    assert(facilitators.size == ids.size)
   }
 
   "The gossip path" should "always be shorter then the total set of node ids" in {
@@ -44,9 +49,46 @@ class PartitionerTest extends AsyncFlatSpecLike with Matchers with BeforeAndAfte
     assert(pathLengths.forall(_ < ids.size))
   }
 
-  "ShortestPathPartitioner.shortestPath" should "always return shortest path" in {
-    val pathLengths = randomTxs.map(gossipPath(ids, _).size)
-    pathLengths.foreach(println)
-    assert(pathLengths.forall(_ < ids.size))
+  "HausdorffPartition.nerve" should "deterministically repartition random edges" in {
+    val trustNodes: Seq[TrustNode] = DataGeneration.generateFullyConnectedTestData(ids.size)
+    val tdi: List[TrustDataInternal] = trustNodes.map(tn =>
+      TrustDataInternal(idxId(tn.id), tn.edges.map(e => (idxId(e.dst), e.trust)).toMap)).toList
+    val partitioner: HausdorffPartition = HausdorffPartition(tdi.tail)(tdi.head)
+    val partitions: Map[Int, List[TrustDataInternal]] = partitioner.nerve
+    val repartition: Map[Int, List[TrustDataInternal]] = partitioner.rePartition(tdi.tail)(partitioner.nerve)
+    assert(repartition === partitions)
+  }
+
+  "HausdorffPartition.nerve" should "deterministically repartition clique edges" in {
+    val trustNodes: Seq[TrustNode] = DataGeneration.generateCliqueTestData(ids.size)
+    val tdi: List[TrustDataInternal] = trustNodes.map(tn =>
+      TrustDataInternal(idxId(tn.id), tn.edges.map(e => (idxId(e.dst), e.trust)).toMap)).toList
+    val partitioner: HausdorffPartition = HausdorffPartition(tdi.tail)(tdi.head)
+    val partitions: Map[Int, List[TrustDataInternal]] = partitioner.nerve
+    val repartition: Map[Int, List[TrustDataInternal]] = partitioner.rePartition(tdi.tail)(partitioner.nerve)
+    assert(repartition === partitions)
+  }
+
+  "HausdorffPartition.nerve" should "not repeat peers in partition paths" in {
+    val trustNodes: Seq[TrustNode] = DataGeneration.generateFullyConnectedTestData(ids.size)
+    val tdi: List[TrustDataInternal] = trustNodes.map(tn =>
+      TrustDataInternal(idxId(tn.id), tn.edges.map(e => (idxId(e.dst), e.trust)).toMap)).toList
+    val partitioner: HausdorffPartition = HausdorffPartition(tdi.tail)(tdi.head)
+    val partitions: Map[Int, List[TrustDataInternal]] = partitioner.nerve
+    assert((1 until partitions.size - 2).forall { rank =>
+      partitions(rank).intersect(partitions(rank + 1)).isEmpty
+    })
+  }
+
+  "HausdorffPartition.nerve" should "deterministically layer bipartite cliques" in {
+    val trustNodes: Seq[TrustNode] = DataGeneration.generateBipartiteTestData(ids.size)
+    val tdi: List[TrustDataInternal] = trustNodes.map(tn =>
+      TrustDataInternal(idxId(tn.id), tn.edges.map(e => (idxId(e.dst), e.trust)).toMap)).toList
+    val (lPartite, rPartite) = tdi.tail.zipWithIndex.partition { case (_, idx) => idx < (ids.size / 2) }
+    val partitioner: HausdorffPartition = HausdorffPartition(tdi.tail)(tdi.head)
+    val partitions: Map[Int, List[TrustDataInternal]] = partitioner.nerve
+    val rank1Eq: Boolean = partitions(1).map(_.id).toSet == lPartite.map(_._1.id).toSet
+    val rank2Eq: Boolean = partitions(2).map(_.id).toSet == rPartite.map(_._1.id).toSet
+    assert(rank1Eq && rank2Eq)
   }
 }
